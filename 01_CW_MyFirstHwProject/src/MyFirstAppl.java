5cc78a1269669ab1daadab27c33eb6e88fb99e9f
xcplaza <xcplaza@gmail.com>
2023-04-05 18:08:38 +0300

    01_First_project

---
+++ 01_CW_MyFirstHwProject/src/MyFirstAppl.java
@@ -0,0 +1,19 @@
+
+public class MyFirstAppl {
+
+	public static void main(String[] args) {
+//		 1 option
+		System.out.println("I am a programmer!\n");
+		System.out.println("Hello\n");
+		System.out.println("i\n");
+		System.out.println("am\n");
+		System.out.println("Elchin\n");
+		
+//		 2 option
+		System.out.print("I am a programmer!\n\nHello\n\ni\n\nam\n\nElchin\n");
+		
+//		3 option
+		System.out.printf("%s\n\n%s\n\n%s\n\n%s\n\n%s\n\n", "I am a programmer!", "Hello", "i", "am", "Elchin");
+	}
+
+}
