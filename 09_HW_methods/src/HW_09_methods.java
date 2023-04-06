f4e4aaae5d57e9b79a8d8f39e3b881fe38a2c2d2
xcplaza <xcplaza@gmail.com>
2023-04-07 00:32:21 +0300

    Update HW_09_methods.java

--- 09_HW_methods/src/HW_09_methods.java
+++ 09_HW_methods/src/HW_09_methods.java
@@ -4,23 +4,27 @@ public class HW_09_methods {
 	public static void main(String[] args) {
 		int number = 1234560;
 		System.out.println("sum = " + sumOddDigits(number));
-		
+
 		number = 5;
 		System.out.println("fac = " + factorial(number));
 
-		number = 1203;
+		number = 123456;
 		printReverseNumber(number);
 
 		number = 12340;
-		System.out.println("revers!0 = " + reverseNumber(number));
+		System.out.println("revers !0 = " + reverseNumber(number));
 
-		number = 12340;
+		number = 1234001234;
 		System.out.println("luckyNum = " + isLuckyNumber(number));
 
 	}
 
 //1
 	public static int sumOddDigits(int number) {
+		if (number < 0) {
+			System.out.print("number error / ");
+			return -1;
+		}
 		int sum = 0;
 		while (number != 0) {
 			int digit = number % 10;
@@ -34,8 +38,9 @@ public class HW_09_methods {
 
 //2
 	public static int factorial(int number) { // (5!-> 1*2*3*4*5-> 120);
-		if (number == 0) {
-			return 1;
+		if (number <= 0) {
+			System.out.print("factorial error / ");
+			return -1;
 		}
 		int result = 1;
 		int i = 1;
@@ -48,6 +53,10 @@ public class HW_09_methods {
 
 //3
 	public static void printReverseNumber(int number) {
+		if (number < 0) {
+			System.out.println("revers error");
+			return;
+		}
 		int result = 0;
 		while (number != 0) {
 			result = result * 10 + number % 10;
@@ -59,7 +68,7 @@ public class HW_09_methods {
 //4
 	public static int reverseNumber(int number) {
 		if (number < 0 || number % 10 == 0) {
-			System.out.print ("number end 0! / ");
+			System.out.print("number end 0! / ");
 			return -1;
 		}
 		int result = 0;
@@ -74,51 +83,39 @@ public class HW_09_methods {
 	public static boolean isLuckyNumber(int number) {
 		int digitCount = 0;
 		int temp = number;
-
-		// считаем количество цифр в числе
-		while (temp > 0) {
-			temp /= 10;
+		while (temp != 0) {
 			digitCount++;
+			temp /= 10;
 		}
-
-		// исключаем цифру посередине, если число имеет нечетное количество цифр
-		boolean excludeMiddle = false;
-		if (digitCount % 2 != 0) {
-			excludeMiddle = true;
+		if (digitCount < 10) {
+			System.out.print("number error / ");
+			return false;
 		}
-
-		// вычисляем сумму первой половины числа
-		int halfSum = 0;
-		int power = 1;
-		int digit;
-		while (number > 0) {
-			digit = number % 10;
-			if (excludeMiddle && power == (digitCount / 2) + 1) {
-				// исключаем цифру посередине, если число имеет нечетное количество цифр
-				excludeMiddle = false;
-			} else if (power <= digitCount / 2) {
-				halfSum += digit;
-			}
-			number /= 10;
-			power *= 10;
+		int half1 = 0;
+		int half2 = 0;
+		int divisor = 1;
+		int count = digitCount / 2;
+		while (count > 0) {
+			divisor *= 10;
+			count--;
 		}
-
-		// вычисляем сумму второй половины числа
-		int secondHalfSum = (digitCount % 2 == 0) ? halfSum : 0; // если количество цифр четное, вторая половина
-																	// суммируется сразу же
-		number = number * power / 10; // восстанавливаем число, отбросив последнюю цифру
-		power = 1;
-		while (number > 0) {
-			digit = number % 10;
-			if (power > digitCount / 2) {
-				secondHalfSum += digit;
-			}
-			number /= 10;
-			power *= 10;
+		if (digitCount % 2 == 0) {
+			half1 = number / divisor;
+			half2 = number % divisor;
+		} else {
+			half1 = number / divisor;
+			half2 = number % (divisor * 10);
 		}
-
-		// проверяем равенство сумм половин числа
-		return halfSum == secondHalfSum;
+		int sum1 = 0;
+		int sum2 = 0;
+		while (half1 != 0) {
+			sum1 += half1 % 10;
+			half1 /= 10;
+		}
+		while (half2 != 0) {
+			sum2 += half2 % 10;
+			half2 /= 10;
+		}
+		return sum1 == sum2;
 	}
-
 }
