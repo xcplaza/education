c0d60f3f0487a0ffd227362f26107d6303640be2
xcplaza <xcplaza@gmail.com>
2023-04-09 12:15:34 +0300

    10_CW_for

--- 09_HW_methods/src/HW_09_methods.java
+++ 09_HW_methods/src/HW_09_methods.java
@@ -1,3 +1,4 @@
+import java.util.concurrent.CountDownLatch;
 
 public class HW_09_methods {
 
@@ -5,8 +6,8 @@ public class HW_09_methods {
 		int number = 1234560;
 		System.out.println("sum = " + sumOddDigits(number));
 
-		number = 5;
-		System.out.println("fac = " + factorial(number));
+		number = 0;
+		System.out.println(number + "! = " + factorial(number));
 
 		number = 123456;
 		printReverseNumber(number);
@@ -14,7 +15,7 @@ public class HW_09_methods {
 		number = 1234;
 		System.out.println("revers !0 = " + reverseNumber(number));
 
-		number = 1234001234;
+		number = 3939456;
 		System.out.println("luckyNum = " + isLuckyNumber(number));
 
 	}
@@ -23,7 +24,7 @@ public class HW_09_methods {
 	public static int sumOddDigits(int number) {
 		if (number < 0) {
 			System.out.print("number error / ");
-			return -1;
+			return number;
 		}
 		int sum = 0;
 		while (number != 0) {
@@ -37,41 +38,67 @@ public class HW_09_methods {
 	}
 
 //2
+//	public static int factorial(int number) {
+//		if (number == 0) {
+//			return 1;
+//		} else if (number < 0 || number > 12) {
+//			System.out.print("factorial error / ");
+//			return -1;
+//		}
+//		int result = 1;
+//		int i = 1;
+//		while (i <= number) {
+//			result *= i;
+//			i++;
+//		}
+//		return result;
+//	}
+
 	public static int factorial(int number) {
-		if (number == 0) {
-			return 1;
-		} else if (number < 0) {
+		if (number < 0 || number > 12) {
 			System.out.print("factorial error / ");
 			return -1;
+
 		}
 		int result = 1;
-		int i = 1;
-		while (i <= number) {
-			result *= i;
-			i++;
+		while (number > 1) {
+			result *= number--;
 		}
 		return result;
 	}
 
 //3
+//	public static void printReverseNumber(int number) {
+//		if (number < 0) {
+//			System.out.println("revers error");
+//			return;
+//		}
+//		int result = 0;
+//		while (number != 0) {
+//			result = result * 10 + number % 10;
+//			number /= 10;
+//		}
+//		System.out.println("revers = " + result);
+//	}
+
 	public static void printReverseNumber(int number) {
 		if (number < 0) {
 			System.out.println("revers error");
 			return;
 		}
-		int result = 0;
-		while (number != 0) {
-			result = result * 10 + number % 10;
+		do {
+			System.out.print(number % 10);
 			number /= 10;
-		}
-		System.out.println("revers = " + result);
+		} while (number != 0);
+		System.out.println();
+
 	}
 
 //4
 	public static int reverseNumber(int number) {
 		if (number < 0 || number % 10 == 0) {
 			System.out.print("number end 0! / ");
-			return -1;
+			return number;
 		}
 		int result = 0;
 		while (number != 0) {
@@ -83,41 +110,81 @@ public class HW_09_methods {
 
 //5
 	public static boolean isLuckyNumber(int number) {
-		int digitCount = 0;
-		int temp = number;
-		while (temp != 0) {
-			digitCount++;
-			temp /= 10;
-		}
-		if (digitCount < 10) {
-			System.out.print("number error / ");
+		if (number <= 10) // количество по значению
 			return false;
-		}
-		int half1 = 0;
-		int half2 = 0;
-		int divisor = 1;
-		int count = digitCount / 2;
-		while (count > 0) {
-			divisor *= 10;
-			count--;
-		}
-		if (digitCount % 2 == 0) {
-			half1 = number / divisor;
-			half2 = number % divisor;
-		} else {
-			half1 = number / divisor;
-			half2 = number % (divisor * 10);
-		}
-		int sum1 = 0;
-		int sum2 = 0;
-		while (half1 != 0) {
-			sum1 += half1 % 10;
-			half1 /= 10;
-		}
-		while (half2 != 0) {
-			sum2 += half2 % 10;
-			half2 /= 10;
-		}
-		return sum1 == sum2;
+		int countDigits = countDigits(number);
+		int divider = xPower(10, countDigits / 2); // степень числа
+		int left = number / divider; // получаем левую часть 1239456 / 1000 = 1239
+		if (countDigits % 2 != 0) // если нечетное количество цифр делением убираем полседнюю цифру
+			left /= 10;
+		int right = number % divider; // получаем правую часть 123456 %1000 = 456
+		return sumDigits(left) == sumDigits(right); // сравниваем
 	}
+
+	private static int sumDigits(int num) {
+		int sum = 0;
+		while (num != 0) {
+			sum += num % 10;
+			num /= 10;
+		}
+		return sum;
+	}
+
+	private static int countDigits(int num) {
+		int count = 0;
+		do {
+			count++;
+			num /= 10;
+		} while (num != 0);
+
+		return count;
+	}
+
+	private static int xPower(int x, int power) {
+		int res = 1;
+		while (power > 0) {
+			res *= x;
+			power--;
+		}
+		return res;
+	}
+
+//	public static boolean isLuckyNumber(int number) {
+//		int digitCount = 0;
+//		int temp = number;
+//		while (temp != 0) {
+//			digitCount++;
+//			temp /= 10;
+//		}
+//		if (digitCount < 10) {
+//			System.out.print("number error / ");
+//			return false;
+//		}
+//		int half1 = 0;
+//		int half2 = 0;
+//		int divisor = 1;
+//		int count = digitCount / 2;
+//		while (count > 0) {
+//			divisor *= 10;
+//			count--;
+//		}
+//		if (digitCount % 2 == 0) {
+//			half1 = number / divisor;
+//			half2 = number % divisor;
+//		} else {
+//			half1 = number / divisor;
+//			half2 = number % (divisor * 10);
+//		}
+//		int sum1 = 0;
+//		int sum2 = 0;
+//		while (half1 != 0) {
+//			sum1 += half1 % 10;
+//			half1 /= 10;
+//		}
+//		while (half2 != 0) {
+//			sum2 += half2 % 10;
+//			half2 /= 10;
+//		}
+//		return sum1 == sum2;
+//	}
 }
---
+++ 10_CW_for/src/CW_10_ForInr.java
@@ -0,0 +1,110 @@
+
+public class CW_10_ForInr {
+
+	public static void main(String[] args) {
+		int a;
+		for (a = 0; a < 5; a++) {
+			System.out.print(a + " ");
+		}
+		System.out.println();
+//=============================
+		a = 0;
+		for (; a < 5; a++) {
+			System.out.print(a + " ");
+		}
+		System.out.println();
+//=============================
+		a = 0;
+		for (; a < 5;) {
+			System.out.print(a + " ");
+			a++;
+		}
+		System.out.println();
+//=============================
+		a = 0;
+		for (;;) {
+			System.out.print(a + " ");
+			a++;
+			if (a == 5)
+				break;
+		}
+		System.out.println();
+//=============================
+		for (int c = 1, b = 0; c <= 10 && b <= 100; c++, b += 10) {
+			System.out.println("c = " + c + ", b = " + b);
+		}
+		// =============================
+		a = 123456;
+		System.out.println(a + " -> " + backOrder(a));
+
+		// =============================
+		int num = 12345, digit = 4;
+		System.out.println(hasDigit(num, digit));
+
+		// ============================= ищем число 56 в num
+		digit = 34;
+		System.out.println(hasSubNumber(num, digit));
+
+	}
+
+	public static int backOrder(int num) {
+		if (num < 0 || num % 10 == 0) {
+			System.out.print("number end 0! / ");
+			return num;
+		}
+		int res;
+		for (res = 0; num != 0; num /= 10) {
+			res = res * 10 + num % 10;
+		}
+		return res;
+	}
+
+	public static boolean hasDigit(int num, int digit) {
+		if (num < 0 || digit < 0 || digit > 9) {
+			System.out.println("wrong args");
+			return false;
+		}
+		do {
+			if (num % 10 == digit)
+				return true;
+			num /= 10;
+		} while (num != 0);
+		return false;
+	}
+
+	public static boolean hasSubNumber(int num, int subNum) { // 1234567 sub=56
+		if (num < 0 || subNum < 0 || subNum > num) {
+			System.out.print("number end 0! / ");
+			return false;
+		}
+
+		int dCount = countDigits(subNum);
+		int divider = xPower(10, dCount);
+
+		do {
+			if (num % divider == subNum)
+				return true;
+			num /= 10;
+		} while (num != 0);
+		return false;
+	}
+
+	private static int countDigits(int num) {
+		int count = 0;
+		do {
+			count++;
+			num /= 10;
+		} while (num != 0);
+		return count;
+	}
+
+	private static int xPower(int x, int power) {
+		int res = 1;
+		while (power > 0) {
+			res *= x;
+			power--;
+		}
+		return res;
+	}
+
+}
