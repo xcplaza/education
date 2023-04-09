ab960e90383c79856447161815b66dfba4a4b6c4
xcplaza <xcplaza@gmail.com>
2023-04-09 16:32:34 +0300

    Update calcEnergy.java

--- CalcEnergy/src/calcEnergy.java
+++ CalcEnergy/src/calcEnergy.java
@@ -4,26 +4,32 @@ import java.util.Scanner;
 public class calcEnergy {
 
 	public static void main(String[] args) {
-//		Scanner pk = new Scanner(System.in);
-//		System.out.println("Введи предыдущее значение kWt = ?");
-//		int pkWt = pk.nextInt();
-		int pkWt = 15794;
-		
-		Scanner k = new Scanner(System.in);
-		System.out.print("Введи текущее значение kWt = ");
-		int kWt = k.nextInt();
-//		int kWt = 16388;
-		
+		double feb = 6.8; // c февраля скидка на оплату 6.8%
+
 //		System.out.println("Введи стоимость (в огоротах) = ?");
 //		Scanner c = new Scanner(System.in);
 //		int cost = c.nextInt();
 		double cost = 66.55;
-		System.out.println("Текущая стоимость за 1кВт " + cost);
-		
+		System.out.println("Стоимость 1кВт в 2022 = " + cost);
+
+//		Scanner pk = new Scanner(System.in);
+//		System.out.println("Введи предыдущее значение kWt = ?");
+//		int pkWt = pk.nextInt();
+		int pkWt = 15794;
+
+		Scanner k = new Scanner(System.in);
+		System.out.print("Введи текущее значение kWt = ");
+		int kWt = k.nextInt();
+//		int kWt = 16388;
+
 		double extCost = cost / 100;
+		double perCost = (extCost * feb / 100);
 		double sum = (kWt - pkWt) * extCost;
-		System.out.printf("Сумма к оплате = %.2f", sum);
+		double percent = (kWt - pkWt) * perCost;
+		double perSum = sum - percent;
+		System.out.printf("Сумма к оплате в 2022= %.2f\n", sum);
+		System.out.printf("Сумма к оплате в 2023= %.2f\n", perSum);
 
 	}
-
+	
 }
