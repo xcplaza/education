9d6743c8ccf5156018c2b0ab24fc93b7bee7e38a
xcplaza <xcplaza@gmail.com>
2023-04-17 14:08:16 +0300

    Update HW_10_Numbers.java

--- 10_HW_for/src/HW_10_Numbers.java
+++ 10_HW_for/src/HW_10_Numbers.java
@@ -4,9 +4,11 @@ public class HW_10_Numbers {
 	public static void main(String[] args) {
 		int number = 1340536340;
 		System.out.println("luckyNum = " + isLuckyNumberPiter(number));
+		System.out.println("LuckyNum = " + isLuckyNumberPiter2(number));
 
 		int subNumber = 340;
 		System.out.println("Number = " + number + " SubNumber = " + subNumber + " Count = " + countSubNumber(number, subNumber));
+		System.out.println("Number = " + number + " SubNumber = " + subNumber + " Count = " + countSubNumber2(number, subNumber));
 
 	}
 
@@ -28,12 +30,27 @@ public class HW_10_Numbers {
 		}
 		return eveNum == oddNum;
 	}
+//1_1
+	public static boolean isLuckyNumberPiter2(int number) {
+		if (number <= 10) {
+			return false;
+		}
+		int res = 0, temp;
+		for (; number != 0; number /= 10) {
+			temp = number % 10;
+			if (temp % 2 == 0) {
+				res += temp;
+			} else
+				res -= temp;
+		}
+		return res == 0;
+	}
 
 //	2 Возвращает сколько раз суб число в числе встречается или возвращает 0 если не найдено
 	public static int countSubNumber(int number, int subNumber) {
-		if (number < 0 || subNumber < 0 || subNumber > number) {
+		if (subNumber < 0 || subNumber > number) {
 			System.out.println("Wrong number or subNumber");
-			return 0;
+			return -1;
 		}
 		int count = 0;
 		int subNumberDig = countDigits(subNumber);
@@ -53,6 +70,23 @@ public class HW_10_Numbers {
 				number /= 10;
 			}
 		}
+		return count;
+	}
+	
+//	2_2
+	public static int countSubNumber2(int number, int subNumber) {
+		if (subNumber < 0 || subNumber > number) {
+			System.out.println("Wrong number or subNumber");
+			return -1;
+		}
+		int dCount = countDigits (subNumber);
+		int divider = xPower(10, dCount);
+		int count = 0;
+		do {
+			if (number % divider == subNumber)
+				count++;
+			number /=10;
+		} while (number !=10);
 		return count;
 	}
 
