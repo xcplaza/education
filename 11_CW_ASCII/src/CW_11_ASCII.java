6965c0e0185991b8a263ab8ea138db77ed7ce7a4
xcplaza <xcplaza@gmail.com>
2023-04-23 22:37:43 +0300

    Create CW_11_ASCII.java

---
+++ 11_CW_ASCII/src/CW_11_ASCII.java
@@ -0,0 +1,45 @@
+
+public class CW_11_ASCII {
+
+	public static void main(String[] args) {
+		char ch = 'P';
+		System.out.println(ch);
+		System.out.println((int) ch);
+//		ch = (char) (ch + 4);
+		ch += 4;
+		System.out.println(ch);
+
+		ch = 123;
+		System.out.println(ch);
+
+//		printASCII();
+
+		char start = 'A';
+		char finish = 'z';
+		int col = 4;
+		printASCIIRange(start, finish, col);
+	}
+
+	public static void printASCIIRange(char start, char finish, int col) {
+		if (start < 32 || finish > 126 || finish < start || col < 1) {
+			System.out.println("Wrong args");
+			return;
+		}
+		for (int count = 1; start <= finish; start++, count++) {
+			System.out.print((int)start + " -> " + start + " \t");
+			if (count % col == 0)
+				System.out.println();
+		}
+		System.out.println();
+
+	}
+
+	public static void printASCII() {
+		for (char count = 32; count < 127; count++) {
+			System.out.print((int) count + " -> " + count + " \t"); // 80 -> P
+			if (count % 3 == 1)
+				System.out.println();
+		}
+		System.err.println();
+	}
+}
