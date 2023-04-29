9155d1789b947fca2a3e3ac716e0a82539316c31
xcplaza <xcplaza@gmail.com>
2023-04-29 14:50:35 +0300

    Update HW_13_Arrays.java

--- 13_HW_Arrays/src/HW_13_Arrays.java
+++ 13_HW_Arrays/src/HW_13_Arrays.java
@@ -44,7 +44,7 @@ public class HW_13_Arrays {
 			return -1;
 		}
 		int max = 0;
-		for (int i = 0; i < arr.length; i++) {
+		for (int i = 1; i < arr.length; i++) {
 			if (arr[i] > arr[max])
 				max = i;
 		}
