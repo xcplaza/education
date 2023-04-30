8d3a03057e9c066963f55f9edb812602cb7583d9
xcplaza <xcplaza@gmail.com>
2023-04-30 15:49:37 +0300

    Update HW_13_Arrays.java

--- 13_HW_Arrays/src/HW_13_Arrays.java
+++ 13_HW_Arrays/src/HW_13_Arrays.java
@@ -12,12 +12,12 @@ public class HW_13_Arrays {
 		System.out.println();
 
 		int[] arAVG = new int[10];
-		int minAVG = 0, maxAVG = 5;
-		if (minAVG < 0) {
-		    System.out.println("Wrong parameters: minAVG!\n");
-		} else {
-		    randomArrays(arAVG, minAVG, maxAVG);
-		}
+		int minAVG = 1, maxAVG = 5;
+//		if (minAVG < 0) {
+//			System.out.println("Wrong parameters: minAVG!\n");
+//		} else {
+		randomArrays(arAVG, minAVG, maxAVG);
+//		}
 		System.out.println("RandomArraysAVG = " + Arrays.toString(arAVG));
 		System.out.println("2. AVG = " + avg(arAVG));
 		System.out.println();
@@ -39,8 +39,8 @@ public class HW_13_Arrays {
 
 // 1
 	public static int searchMax(int[] arr) {
-		if (arr == null) {
-			System.out.println("Arrays is null!");
+		if (arr == null || arr.length == 0) {
+			System.out.println("Arrays is null or empty!");
 			return -1;
 		}
 		int max = 0;
@@ -52,21 +52,38 @@ public class HW_13_Arrays {
 	}
 
 // 2
+
 	public static float avg(int[] ar) {
-		if (ar == null) {
-			System.out.println("Wrong parameters!");
-			return -1;
+		if (ar == null || ar.length == 0) {
+			System.out.println("Arrays is null or empty!");
+			return Float.NaN;
 		}
-		float average = 0;
-		if (ar.length > 0) {
-			float sum = 0;
-			for (int i = 0; i < ar.length; i++) {
-				sum += ar[i];
+		float sum = 0.f;
+		for (int i = 0; i < ar.length; i++) {
+			if (ar[i] <= 0) {
+				System.out.println("Wrong parameters: minAVG!\n");
+				return Float.NaN;
 			}
-			average = sum / ar.length;
+			sum += ar[i];
 		}
-		return average;
+		return sum / ar.length;
 	}
+
+//	public static float avg(int[] ar) {
+//		if (ar == null) {
+//			System.out.println("Arrays is null!");
+//			return Float.NaN;
+//		}
+//		float average = 0;
+//		if (ar.length > 0) {
+//			float sum = 0;
+//			for (int i = 0; i < ar.length; i++) {
+//				sum += ar[i];
+//			}
+//			average = sum / ar.length;
+//		}
+//		return average;
+//	}
 
 // 3
 	public static void reverseArray(int[] ar) {
@@ -74,32 +91,71 @@ public class HW_13_Arrays {
 			System.out.println("Arrays is null!");
 			return;
 		}
-		int length = ar.length;
-		for (int i = 0; i < length / 2; i++) {
-			int temp = ar[i];
-			ar[i] = ar[length - i - 1];
-			ar[length - i - 1] = temp;
+		int temp = 0;
+		for (int i = 0, j = ar.length - 1; i < j; i++, j--) {
+			temp = ar[i];
+			ar[i] = ar[j];
+			ar[j] = temp;
 		}
 	}
 
+//	public static void reverseArray(int[] ar) {
+//		if (ar == null) {
+//			System.out.println("Arrays is null!");
+//			return;
+//		}
+//		int length = ar.length;
+//		for (int i = 0; i < length / 2; i++) {
+//			int temp = ar[i];
+//			ar[i] = ar[length - i - 1];
+//			ar[length - i - 1] = temp;
+//		}
+//	}
+
 // 4
 	public static void firstLastPosSum(int[] ar) {
-		int firstIndex = -1;
-		int lastIndex = -1;
+		if (ar == null) {
+			System.out.println("Arrays is null!");
+			return;
+		}
+		int firstPos = -1, lastPos = -1;
 		for (int i = 0; i < ar.length; i++) {
 			if (ar[i] > 0) {
-				if (firstIndex == -1) {
-					firstIndex = i;
-				} else {
-					lastIndex = i;
-				}
+				firstPos = ar[i];
+				break;
 			}
 		}
-		if (firstIndex != -1 && lastIndex != -1) {
-			int sum = ar[firstIndex] + ar[lastIndex];
-			System.out.println("4. SumArrays: " + sum);
-		} else {
-			System.out.println("4. Arrays is not positive.");
+		if (firstPos == -1) {
+			System.out.println("No pisitive elements");
+			return;
 		}
+		for (int i = ar.length - 1; i >= 0; i--) {
+			if (ar[i] > 0) {
+				lastPos = ar[i];
+				break;
+			}
+		}
+		System.out.println("Sum elements = " + (firstPos + lastPos));
+
 	}
+
+//	public static void firstLastPosSum(int[] ar) {
+//		int firstIndex = -1;
+//		int lastIndex = -1;
+//		for (int i = 0; i < ar.length; i++) {
+//			if (ar[i] > 0) {
+//				if (firstIndex == -1) {
+//					firstIndex = i;
+//				} else {
+//					lastIndex = i;
+//				}
+//			}
+//		}
+//		if (firstIndex != -1 && lastIndex != -1) {
+//			int sum = ar[firstIndex] + ar[lastIndex];
+//			System.out.println("4. SumArrays: " + sum);
+//		} else {
+//			System.out.println("4. Arrays is not positive.");
+//		}
+//	}
 }
