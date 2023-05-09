fe26e22a2b6715dd4fde38b7f6efa6ded7275369
xcplaza <xcplaza@gmail.com>
2023-05-09 21:36:50 +0300

    Create CarAppl.java

---
+++ CarsProject/src/CarAppl.java
@@ -0,0 +1,43 @@
+
+public class CarAppl {
+
+	public static void main(String[] args) {
+
+		Car car1 = new Car("BMW X5", "Black", 2008, 3.0);
+		Car car2 = new Car("Mercedes-Benz C-Class", "Silver", 1990, 2.0);
+		Car car3 = new Car("Audi A1", "White", 2017, 1.5);
+		Car car4 = new Car("Toyota Camry", "Red", 2021, 2.5);
+		Car car5 = new Car("Mustag Musculcar", "Blue", 1945, 3.5);
+
+		Car[] cars = { car1, car2, car3, car4, car5 };
+
+		allCars(cars);
+		System.out.println();
+
+		System.out.println("/////////////////////////////");
+		cars2L(cars);
+
+	}
+
+	/*
+	 * метод выводящий информацию о всех машинах массива на консоль
+	 */
+	public static void allCars(Car[] cars) {
+		for (int i = 0; i < cars.length; i++) {
+			cars[i].display();
+		}
+
+	}
+
+	/*
+	 * метод выводящий на консоль информацию только о тех машинах объем которых
+	 * более 2 литров
+	 */
+	public static void cars2L(Car[] cars) {
+		for (int i = 0; i < cars.length; i++) {
+			if (cars[i].getMotor() >= 2.0)
+				cars[i].display();
+		}
+	}
+
+}
