f70bcd2f96d277c250a1beb3adb6c86d6e463b30
xcplaza <xcplaza@gmail.com>
2023-05-21 16:30:51 +0300

    Update SaleManager.java

--- 19_HW_UpDownCast/src/SaleManager.java
+++ 19_HW_UpDownCast/src/SaleManager.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 
 public class SaleManager extends Employee {
 	double totalSale; // объем продаж,(ставка + бонус)
@@ -51,6 +52,27 @@ public class SaleManager extends Employee {
 	@Override
 	public String toString() {
 		return super.toString() + ", Sale: " + totalSale + ", Bonus: " + bonus;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(bonus, totalSale);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (!(obj instanceof SaleManager))
+			return false;
+		SaleManager other = (SaleManager) obj;
+		return Double.doubleToLongBits(bonus) == Double.doubleToLongBits(other.bonus)
+				&& Double.doubleToLongBits(totalSale) == Double.doubleToLongBits(other.totalSale);
 	}
 
 }
