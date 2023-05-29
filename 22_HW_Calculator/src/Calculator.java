a836fe1876d35725cc0d6f8f7b9fb53220db5dac
xcplaza <xcplaza@gmail.com>
2023-05-29 20:12:19 +0300

    Update Calculator.java

--- 22_HW_Calculator/src/Calculator.java
+++ 22_HW_Calculator/src/Calculator.java
@@ -6,37 +6,28 @@ public class Calculator {
 			System.out.println("Wrong arrays!");
 			return;
 		}
+		double op1 = Double.parseDouble(args[0]);
+		double op2 = Double.parseDouble(args[2]);
+		String op3 = args[1];
+		double result = 0;
 
-		for (int i = 0; i < args.length; i++) {
-			if (args[i] == null) {
-				System.out.println("Arrays is null!");
-				return;
-			}
-			double op1 = Double.parseDouble(args[0]);
-			double op2 = Double.parseDouble(args[2]);
-			String op3 = args[1];
-			double result = 0;
-
-			switch (op3) {
-			case "+":
-				result = op1 + op2;
-				break;
-			case "-":
-				result = op1 - op2;
-				break;
-			case "x":
-				result = op1 * op2;
-				break;
-			case "/":
-				result = op1 / op2;
-				break;
-			default:
-				System.out.println("Wrong value: " + op3);
-			}
-			System.out.println(result);
-
+		switch (op3) {
+		case "+":
+			result = op1 + op2;
+			break;
+		case "-":
+			result = op1 - op2;
+			break;
+		case "x":
+			result = op1 * op2;
+			break;
+		case "/":
+			result = op1 / op2;
+			break;
+		default:
+			System.out.println("Wrong value: " + op3);
 		}
+		System.out.println(result);
 
 	}
-
 }
