83963280dd39dd473b3c2789d4173941a4fcb65c
xcplaza <xcplaza@gmail.com>
2023-06-14 16:30:44 +0300

    Update StringExpressions.java

--- 25_HW_RegExp/src/StringExpressions.java
+++ 25_HW_RegExp/src/StringExpressions.java
@@ -3,31 +3,58 @@
 public class StringExpressions {
 
 	public static boolean isProgrammingName(String str) {
+		if (str == null || str.isBlank())
+			return false;
 		// allowed letters - Any English letter
 		// allowed digits - any digit
 		// allowed symbols _
 		// first symbol can't be a digit
-		String pattern = "^[a-zA-Z_][\\w]*$";
+		String pattern = "[a-zA-Z_]\\w*";
 		return str.matches(pattern);
 	}
 
 	public static Object isThreeDigitsNumber(String str) {
-		String pattern = "\\b(?!00)\\d{3}";
+		if (str == null || str.isBlank())
+			return false;
+		String pattern = "[1-9]\\d{2}";
 		return str.matches(pattern);
 	}
 
 	public static boolean isWord(String str) {
-		String pattern = "^(?:non\\-w|[Ww])ord$";
+		if (str == null || str.isBlank())
+			return false;
+//		String pattern = "^(?:non\\-w|[Ww])ord$";
+		String pattern = "[a-zA-Z]|([a-zA-Z][a-z]*-?[a-z]+)";
 		return str.matches(pattern);
 	}
 
 	public static boolean isPositiveLessThan1000(String str) {
-		String pattern = "\\b(?!00)\\d{1,3}";
+		if (str == null || str.isBlank())
+			return false;
+//		String pattern = "\\b(?!00)\\d{1,3}";
+		String pattern = "[0]|([1-9]\\d{0,2})";
 		return str.matches(pattern);
 	}
 
 	public static boolean isPositiveLessThan300(String str) {
-		String pattern = "\\d|\\d{1,3}[^300]";
+		if (str == null || str.isBlank())
+			return false;
+//		String pattern = "\\d|\\d{1,3}[^300]";
+		String pattern = "\\d|[1-9]\\d|[12]\\d{2}";
+		return str.matches(pattern);
+	}
+
+	public static boolean isPositiveLessThanEquals255(String str) {
+		if (str == null || str.isBlank())
+			return false;
+		String pattern = "[1-9]\\d?|1\\d{2}|2[0-4]\\d|25[0-5]";
+		return str.matches(pattern);
+	}
+
+	public static boolean isIsraelMobile(String str) {
+		if (str == null || str.isBlank())
+			return false;
+		String pattern = "(\\+972-?5|05)[0-8](-?\\d){7}";
 		return str.matches(pattern);
 	}
 
