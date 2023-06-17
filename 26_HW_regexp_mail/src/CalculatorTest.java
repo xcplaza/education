2df2fae06093c6693a4287ddfd5f9b501328013f
xcplaza <xcplaza@gmail.com>
2023-06-18 00:22:53 +0300

    Create CalculatorTest.java

---
+++ 26_HW_regexp_mail/src/CalculatorTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testWrongExpressions() {
+		assertNull(Calculator.computeExpression("25+100/"));
+		assertNull(Calculator.computeExpression("&25+100"));
+		assertNull(Calculator.computeExpression("25 100"));
+		assertNull(Calculator.computeExpression("25?100"));
+	}
+
+	@Test
+	public void testRightExpressions() {
+		assertEquals((Integer) 10, Calculator.computeExpression(" 10 +10-10"));
+		assertEquals((Integer) 10, Calculator.computeExpression(" 10 *10/10"));
+		assertEquals((Integer) 10, Calculator.computeExpression(" 10 *10/10 +10 -10 "));
+		assertEquals((Integer) 10, Calculator.computeExpression("10*10/10+10-10"));
+	}
+
+}
