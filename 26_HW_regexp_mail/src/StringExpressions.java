dbf2d3cf4ab2991f573a2a07e21453cad3e1c3f8
xcplaza <xcplaza@gmail.com>
2023-06-18 12:14:16 +0300

    Update StringExpressions.java

--- 26_HW_regexp_mail/src/StringExpressions.java
+++ 26_HW_regexp_mail/src/StringExpressions.java
@@ -4,7 +4,7 @@ public class StringExpressions {
 	public static boolean isEmailCoIlMailRu(String str) {
 		if (str == null || str.isBlank())
 			return false;
-		String pattern = "^[^\\s,]+@[-a-zA-Z0-9]+\\.(co\\.il|mail\\.ru)$";
+		String pattern = "[^\\s,]+@([a-zA-Z\\d-]+\\.)?(co\\.il|mail\\.ru)";
 		return str.matches(pattern);
 	}
 
