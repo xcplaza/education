0a3ac39060ac3574427fbb02b45716c433480a5c
xcplaza <xcplaza@gmail.com>
2023-06-22 15:15:23 +0300

    Update IList.java

--- 30_CW_MyArrays/src/IList.java
+++ 30_CW_MyArrays/src/IList.java
@@ -4,4 +4,11 @@ public interface IList {
 	boolean add(int index, Object obj);
 	Object get(int index);
 	int size();
+
+	int indexOf(Object obj);
+	int lastInsdexOf(Object obj);
+	boolean contains(Object obj);
+	Object remove(int index);
+	boolean remove(Object obj);
+	Object[] toArray(); // от 0 до size
 }
