c5cc7a6ecc2535a4e9f2c247a49397eb584a0819
xcplaza <xcplaza@gmail.com>
2023-06-24 20:39:41 +0300

    Update IList.java

--- 30_CW_MyArrays/src/IList.java
+++ 30_CW_MyArrays/src/IList.java
@@ -1,18 +1,18 @@
 
 public interface IList {
-	boolean add(Object obj);
-	boolean add(int index, Object obj);
-	Object get(int index);
-	int size();
+	boolean add(Object obj); // возвращает 
+	boolean add(int index, Object obj); //возвращает true если по указанному индексу есть объект
+	Object get(int index); //метод возвращает объект по индексу
+	int size(); // метод возвращает размер
 
 	int indexOf(Object obj);
-	int lastInsdexOf(Object obj);
+	int lastIndexOf(Object obj);
 	boolean contains(Object obj);
 	Object remove(int index);
 	boolean remove(Object obj);
 	Object[] toArray(); // от 0 до size
 	
-	void addAll(MyArray other) ; // из MyArray other добавить в конец MyArray (наш)
-	void addAll(MyArray other, int index) ; // из MyArray other добавить в MyArray (наш) + сдвигаем и вставляем по index
+	void addAll(MyArray other); // из MyArray other добавить в конец MyArray (наш)
+	void addAll(int index, MyArray other); // из MyArray other добавить в MyArray (наш) + сдвигаем и вставляем по index
 	boolean removeAll(Object obj); // удалить все объекты(передаваемые например 10) и возвратить  true если хотя бы 1 элемент удален
 }
