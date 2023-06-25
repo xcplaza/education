acd467c873950c3b5a543cacd0ce9ce29b0fc848
xcplaza <xcplaza@gmail.com>
2023-06-25 13:22:09 +0300

    Update Box.java

--- 32_CW_VarArgs_Generics/src/Box.java
+++ 32_CW_VarArgs_Generics/src/Box.java
@@ -1,4 +1,27 @@
 
-public class Box {
+public class Box<T> { // типизируем по типу который в дальнейшем захотим использовать
+	/*
+	 * E - Element 
+	 * K - Key 
+	 * V - Value 
+	 * N - Number 
+	 * T - Type 
+	 * <> обозначаем типизацию
+	 */
+	private T value;
+
+	public Box(T value) {
+		setValue(value);
+	}
+
+	public T getValue() {
+		return value;
+	}
+
+	public void setValue(T value) {
+		this.value = value;
+	}
+	
+	
 
 }
