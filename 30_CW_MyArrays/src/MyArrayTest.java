386fb0f487154b608f89d6d4a4414d8e688f65ce
xcplaza <xcplaza@gmail.com>
2023-06-25 11:24:53 +0300

    Update MyArrayTest.java

--- 30_CW_MyArrays/src/MyArrayTest.java
+++ 30_CW_MyArrays/src/MyArrayTest.java
@@ -1,11 +1,12 @@
 import static org.junit.Assert.*;
 
+import java.util.Iterator;
+
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
-public class MyArrayTest
-{
+public class MyArrayTest {
 	IList numbers;
 	IList strings;
 	Integer[] arNumbers = { 10, 7, 11, -2, 13, 10, 2000 };
@@ -15,63 +16,52 @@ public class MyArrayTest
 	Integer[] arOther = { 1, 2, 3, 4 };
 
 	@Before
-	public void setUp() throws Exception
-	{
+	public void setUp() throws Exception {
 		numbers = new MyArray(1);
-		for (int i = 0; i < arNumbers.length; i++)
-		{
+		for (int i = 0; i < arNumbers.length; i++) {
 			numbers.add(arNumbers[i]);
 		}
 		strings = new MyArray();
-		for (int i = 0; i < arStrings.length; i++)
-		{
+		for (int i = 0; i < arStrings.length; i++) {
 			strings.add(arStrings[i]);
 		}
 		other = new MyArray();
-		for (int i = 0; i < arOther.length; i++)
-		{
+		for (int i = 0; i < arOther.length; i++) {
 			other.add(arOther[i]);
 		}
 	}
 
 	@Test
-	public void testSetUp()
-	{
+	public void testSetUp() {
 		int sizeNumbers = numbers.size();
 		int sizeStrings = strings.size();
 		assertEquals(arNumbers.length, sizeNumbers);
 		assertEquals(arStrings.length, sizeStrings);
-		for (int i = 0; i < sizeNumbers; i++)
-		{
+		for (int i = 0; i < sizeNumbers; i++) {
 			assertEquals(arNumbers[i], numbers.get(i));
 		}
-		for (int i = 0; i < sizeStrings; i++)
-		{
+		for (int i = 0; i < sizeStrings; i++) {
 			assertEquals(arStrings[i], strings.get(i));
 		}
 	}
 
 	@Test
-	public void testAddToIndex()
-	{
+	public void testAddToIndex() {
 		Integer[] exp = { 100, 10, 7, 11, -2, 13, 10, 2000 };
 		assertTrue(numbers.add(0, 100));
-		for (int i = 0; i < numbers.size(); i++)
-		{
+		for (int i = 0; i < numbers.size(); i++) {
 			assertEquals(exp[i], numbers.get(i));
 		}
 
 		Integer[] exp1 = { 100, 10, 7, 100, 11, -2, 13, 10, 2000 };
 		assertTrue(numbers.add(3, 100));
-		for (int i = 0; i < numbers.size(); i++)
-		{
+		for (int i = 0; i < numbers.size(); i++) {
 			assertEquals(exp1[i], numbers.get(i));
 		}
 
 		Integer[] exp2 = { 100, 10, 7, 100, 11, -2, 13, 10, 2000, 100 };
 		assertTrue(numbers.add(numbers.size(), 100));
-		for (int i = 0; i < numbers.size(); i++)
-		{
+		for (int i = 0; i < numbers.size(); i++) {
 			assertEquals(exp2[i], numbers.get(i));
 		}
 
@@ -81,8 +71,7 @@ public class MyArrayTest
 	}
 
 	@Test
-	public void testIndexOf()
-	{
+	public void testIndexOf() {
 		assertEquals(0, numbers.indexOf(10));
 		assertEquals(-1, numbers.indexOf(100));
 		assertEquals(-1, numbers.indexOf(null));
@@ -94,8 +83,7 @@ public class MyArrayTest
 	}
 
 	@Test
-	public void testLastIndexOf()
-	{
+	public void testLastIndexOf() {
 		assertEquals(5, numbers.lastIndexOf(10));
 		assertEquals(-1, numbers.lastIndexOf(100));
 		assertEquals(-1, numbers.lastIndexOf(null));
@@ -106,8 +94,7 @@ public class MyArrayTest
 	}
 
 	@Test
-	public void testContains()
-	{
+	public void testContains() {
 		assertTrue(numbers.contains(10));
 		assertFalse(numbers.contains(100));
 		assertFalse(numbers.contains(null));
@@ -118,38 +105,32 @@ public class MyArrayTest
 	}
 
 	@Test
-	public void testRemoveAtIndex()
-	{
+	public void testRemoveAtIndex() {
 		Integer[] exp = { 10, 7, -2, 13, 10, 2000 };
 		assertEquals(11, numbers.remove(2));
 		assertEquals(null, numbers.remove(100));
 		assertEquals(null, numbers.remove(-1));
-		for (int i = 0; i < numbers.size(); i++)
-		{
+		for (int i = 0; i < numbers.size(); i++) {
 			assertEquals(exp[i], numbers.get(i));
 		}
 	}
 
 	@Test
-	public void testRemoveObject()
-	{
+	public void testRemoveObject() {
 		String[] exp = { "lmn", "fg", "abc" };
 		assertTrue(strings.remove("abc"));
-		for (int i = 0; i < strings.size(); i++)
-		{
+		for (int i = 0; i < strings.size(); i++) {
 			assertEquals(exp[i], strings.get(i));
 		}
 		Integer[] exp1 = { 10, 7, -2, 13, 10, 2000 };
 		assertTrue(numbers.remove((Integer) 11));
-		for (int i = 0; i < numbers.size(); i++)
-		{
+		for (int i = 0; i < numbers.size(); i++) {
 			assertEquals(exp1[i], numbers.get(i));
 		}
 	}
 
 	@Test
-	public void testToArray()
-	{
+	public void testToArray() {
 		assertArrayEquals(arNumbers, numbers.toArray());
 		assertArrayEquals(strings.toArray(), arStrings);
 	}
@@ -157,35 +138,46 @@ public class MyArrayTest
 //	String[] arStrings = {"abc", "lmn", "fg", "abc"};
 
 	@Test
-	public void testAddAll()
-	{
+	public void testAddAll() {
 		Integer[] arExpected = { 10, 7, 11, -2, 13, 10, 2000, 1, 2, 3, 4 };
 		numbers.addAll(other);
 		assertArrayEquals(arExpected, numbers.toArray());
 	}
-	
+
 	@Test
-	public void testAddAllAtIndex()
-	{
+	public void testAddAllAtIndex() {
 		Integer[] arExpected = { 10, 7, 1, 2, 3, 4, 11, -2, 13, 10, 2000 };
 		numbers.addAll(2, other);
 		assertArrayEquals(arExpected, numbers.toArray());
 	}
-	
+
 	@Test
-	public void testRemoveAll()
-	{
-		String[] exp = { "lmn", "fg"};
+	public void testRemoveAll() {
+		String[] exp = { "lmn", "fg" };
 		assertTrue(strings.removeAll("abc"));
-		for (int i = 0; i < strings.size(); i++)
-		{
+		for (int i = 0; i < strings.size(); i++) {
 			assertEquals(exp[i], strings.get(i));
 		}
 		Integer[] exp1 = { 7, 11, -2, 13, 2000 };
 		assertTrue(numbers.removeAll(10));
-		for (int i = 0; i < numbers.size(); i++)
-		{
+		for (int i = 0; i < numbers.size(); i++) {
 			assertEquals(exp1[i], numbers.get(i));
 		}
+	}
+
+	@Test
+	public void testRemoveAll2() {
+		Integer[] arNumbers2 = { 10, 7, 11, -2, 13, 10, 10, 2000 };
+		numbers = new MyArray(1);
+		for (int i = 0; i < arNumbers2.length; i++) {
+			numbers.add(arNumbers2[i]);
+		}
+		Integer[] exp1 = { 7, 11, -2, 13, 2000 };
+		assertTrue(numbers.removeAll(10));
+		for (int i = 0; i < numbers.size(); i++) {
+			assertEquals(exp1[i], numbers.get(i));
+
+		}
+
 	}
 }
