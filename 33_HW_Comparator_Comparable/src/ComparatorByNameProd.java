bd77008043084ba97a1e9296cfdb9d62a8dd636c
xcplaza <xcplaza@gmail.com>
2023-06-26 14:02:53 +0300

    Create ComparatorByNameProd.java

---
+++ 33_HW_Comparator_Comparable/src/ComparatorByNameProd.java
@@ -0,0 +1,11 @@
+import java.util.Comparator;
+
+public class ComparatorByNameProd implements Comparator<Product> {
+
+	@Override
+	public int compare(Product o1, Product o2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
