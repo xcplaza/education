a52ed60658c278ab7506e57ccb171668c544fe78
xcplaza <xcplaza@gmail.com>
2023-06-27 14:18:39 +0300

    Create MyString.java

---
+++ 34_CW_Iterator_Iterable/src/MyString.java
@@ -0,0 +1,32 @@
+import java.util.Iterator;
+
+public class MyString implements Iterable<Character> {
+	private StringBuilder str;
+
+	public MyString(String str) {
+		super();
+		this.str = new StringBuilder(str);
+	}
+
+	public String getStr() {
+		return str.toString();
+	}
+
+	public void setStr(String str) {
+		this.str = new StringBuilder(str);
+	}
+
+	public int size() {
+		return str.length();
+	}
+
+	public void remove(Character c) {
+		str.deleteCharAt(str.toString().indexOf(c));
+	}
+
+	@Override
+	public Iterator<Character> iterator() {
+		return new MyStringIterator(str);
+	}
+
+}
