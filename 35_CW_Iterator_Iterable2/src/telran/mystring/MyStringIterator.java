f1ebfeb0bce322dc603dd9ca63cbf25ad7def9a7
xcplaza <xcplaza@gmail.com>
2023-06-29 17:44:40 +0300

    Create MyStringIterator.java

---
+++ 35_CW_Iterator_Iterable2/src/telran/mystring/MyStringIterator.java
@@ -0,0 +1,26 @@
+package telran.mystring;
+import java.util.Iterator;
+
+public class MyStringIterator implements Iterator<Character> {
+	private StringBuilder str;
+	private int size;
+	private int current;
+
+	public MyStringIterator(StringBuilder str) {
+		super();
+		this.str = str;
+		size = str.length();
+		current = 0;
+	}
+
+	@Override
+	public boolean hasNext() { // можно ли продолжать итерацию
+		return current < size;
+	}
+
+	@Override
+	public Character next() { // возвращает элемент на котором находится и делает шаг вперед
+		return str.charAt(current++);
+	}
+
+}
