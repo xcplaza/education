7c60290ae5d727f25128b7b0eca0f4a531ae5eac
xcplaza <xcplaza@gmail.com>
2023-06-30 12:24:57 +0300

    Create PasswordCheck.java

---
+++ 36_PasswordCheck/src/PasswordCheck.java
@@ -0,0 +1,16 @@
+
+public class PasswordCheck {
+
+	public static final String WRONG_LENGTH_MESSAGE = null;
+	public static final String NO_SPEC_SYMBOL_MESSAGE = null;
+	public static final String NO_DIGIT_MESSAGE = null;
+	public static final String NO_UPPER_CASE_MESSAGE = null;
+	public static final String NO_LOWER_CASE_MESSAGE = null;
+	public static final String WRONG_SYMBOL_MESSAGE = null;
+
+	public static void check(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
