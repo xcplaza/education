3c7109aebc383560f16c9bbd74d57c86484979b2
xcplaza <xcplaza@gmail.com>
2023-07-03 14:41:55 +0300

    Create Rectangle.java

---
+++ 37_CW_Set/src/Rectangle.java
@@ -0,0 +1,41 @@
+import java.util.Objects;
+
+public class Rectangle {
+	int x;
+	int y;
+
+	public Rectangle(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Rectangle))
+			return false;
+		Rectangle other = (Rectangle) obj;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Rectangle [x=" + x + ", y=" + y + "]";
+	}
+
+}
