f31d7f002ead84b0f7b833a7ec3e7f0ad50f7b5a
xcplaza <xcplaza@gmail.com>
2023-07-03 15:12:44 +0300

    Create Person.java

---
+++ 37_CW_Set/src/Person.java
@@ -0,0 +1,34 @@
+
+public class Person {
+	int id;
+	String name;
+	int age;
+
+	public Person(int id, String name, int age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Person))
+			return false;
+		Person other = (Person) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+}
