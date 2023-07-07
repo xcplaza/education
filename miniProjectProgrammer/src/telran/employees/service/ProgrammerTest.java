ce6d59243481c412b2d783a465ad3843fc87e431
xcplaza <xcplaza@gmail.com>
2023-07-07 13:16:44 +0300

    Update ProgrammerTest.java

--- miniProjectProgrammer/src/telran/employees/service/ProgrammerTest.java
+++ miniProjectProgrammer/src/telran/employees/service/ProgrammerTest.java
@@ -17,45 +17,76 @@ public class ProgrammerTest {
 	Map<Integer, Programmer> programmers;
 	ProgrammersMaps s;
 
+	String[] skils1 = { "java", "c++" };
+	String[] skils2 = { "python", "kotlin" };
+	String[] skils3 = { "php" };
+
+	Programmer elik = new Programmer(302, "Elik", skils1, 20000);
+	Programmer vika = new Programmer(303, "Vika", skils2, 20000);
+	Programmer svyat = new Programmer(304, "Svyat", skils3, 20000);
+	Programmer nul = null;
+
 	@Before
 	public void setUp() throws Exception {
 		programmers = new HashMap<>();
 		s = new ProgrammersMaps(programmers);
+
+		s.addProgrammer(svyat);
+		s.addProgrammer(vika);
 	}
-
-	String[] skils1 = { "java", "c++", "pyton", "kotlin" };
-	String[] skils2 = { "java", "pyton", "kotlin" };
-	String[] skils3 = { "java", "c++", "pyton", "kotlin", "php" };
-
-	Programmer elik = new Programmer(302, "Elik", skils1, 3000);
-	Programmer vika = new Programmer(303, "Vika", skils2, 3000);
-	Programmer svyat = new Programmer(304, "Svyat", skils3, 3000);
-	Programmer nul = new Programmer(0, null, null, 0);
 
 	@Test
 	public void testAddProgrammer() {
 		assertEquals(true, s.addProgrammer(elik));
-		assertEquals(true, s.addProgrammer(vika));
-		assertEquals(true, s.addProgrammer(svyat));
-		assertEquals(true, s.addProgrammer(nul));
-		System.out.println(programmers.toString());
+		assertEquals(false, s.addProgrammer(nul));
 	}
 
 	@Test
 	public void removeProgrammer() {
-		assertEquals(true, s.removeProgrammer(302));
-		System.out.println(programmers.toString());
+		assertEquals(true, s.removeProgrammer(303));
+		assertEquals(false, s.removeProgrammer(900));
+		assertEquals(false, s.removeProgrammer(-100));
+
 	}
 
 	@Test
 	public void getProgrammerData() {
-		
-		System.out.println(s.getProgrammerData(303));
+		assertEquals(svyat, s.getProgrammerData(304));
+		assertEquals(null, s.getProgrammerData(900));
+		assertEquals(null, s.getProgrammerData(-100));
 	}
-	
+
 	@Test
 	public void addNewTechnology() {
-		System.out.println(s.getProgrammerData(303));
+		assertEquals(true, s.addNewTechnology(304, "java"));
+		assertEquals(false, s.addNewTechnology(304, "php"));
+		assertEquals(false, s.addNewTechnology(900, "java"));
+		assertEquals(false, s.addNewTechnology(304, ""));
+		assertEquals(false, s.addNewTechnology(-100, "java"));
+	}
+
+	@Test
+	public void removeTechnology() {
+		assertEquals(true, s.removeTechnology(304, "php"));
+		assertEquals(false, s.removeTechnology(304, "java"));
+		assertEquals(false, s.removeTechnology(900, "java"));
+		assertEquals(false, s.removeTechnology(304, ""));
+		assertEquals(false, s.removeTechnology(-100, "java"));
+//		System.out.println(s.addNewTechnology(304, "java"));
+	}
+
+	@Test
+	public void updateSalary() {
+		assertEquals(true, s.updateSalary(304, 25000));
+		assertEquals(false, s.updateSalary(900, 25000));
+		assertEquals(false, s.updateSalary(900, -10000));
+		assertEquals(false, s.updateSalary(304, 0));
+		assertEquals(false, s.updateSalary(-100, 25000));
+	}
+
+	@Test
+	public void getProgrammersWithTechnology() {
+		System.out.println(s.getProgrammersWithTechnology("java"));
 	}
 
 }
