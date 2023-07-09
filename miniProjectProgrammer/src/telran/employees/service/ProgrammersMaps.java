26ac8f995767b583d5bebe0759579e49886fbd97
xcplaza <xcplaza@gmail.com>
2023-07-09 19:28:14 +0300

    Update ProgrammersMaps.java

--- miniProjectProgrammer/src/telran/employees/service/ProgrammersMaps.java
+++ miniProjectProgrammer/src/telran/employees/service/ProgrammersMaps.java
@@ -17,8 +17,8 @@ import telran.employees.dto.Programmer;
 public class ProgrammersMaps implements IProgrammer {
 
 	private Map<Integer, Programmer> programmers = new HashMap<>();
-	private HashMap<String, List<Programmer>> techProgrammers;
-	private TreeMap<Integer, List<Programmer>> salaryProgrammers;
+	private HashMap<String, List<Programmer>> techProgrammers = new HashMap<>();
+	private TreeMap<Integer, List<Programmer>> salaryProgrammers = new TreeMap<>();
 
 	public ProgrammersMaps(Map<Integer, Programmer> programmers) {
 		super();
@@ -29,19 +29,25 @@ public class ProgrammersMaps implements IProgrammer {
 	public boolean addProgrammer(Programmer programmer) {
 		if (programmer == null)
 			return false;
-		String a =  programmer.getTechnologies().toString();
-	    techProgrammers.put(a,getProgrammersWithTechnology(a));
-	    salaryProgrammers.put(programmer.getId(), getProgrammersWithTechnology(a));
-	  
-	    return programmers.putIfAbsent(programmer.getId(), programmer) == null ? true : false;
-	  }
+		for (String str : programmer.getTechnologies()) {
+			techProgrammers.putIfAbsent(str, new ArrayList<>());
+			techProgrammers.get(str).add(programmer);
+		}
+		salaryProgrammers.putIfAbsent(programmer.getSalary(), new ArrayList<>());
+		salaryProgrammers.get(programmer.getSalary()).add(programmer);
+		return programmers.putIfAbsent(programmer.getId(), programmer) == null ? true : false;
+	}
 
 	@Override
 	public boolean removeProgrammer(int id) {
 		if (id < 0 || !(programmers.containsKey(id)))
 			return false;
+		Programmer rmv = getProgrammerData(id);
+		for (String tech : rmv.getTechnologies())
+			techProgrammers.get(tech).remove(rmv);
+		salaryProgrammers.remove(id);
 		return programmers.remove(id) != null ? true : false;
-		
+
 	}
 
 	@Override
@@ -55,6 +61,8 @@ public class ProgrammersMaps implements IProgrammer {
 	public boolean addNewTechnology(int id, String technology) {
 		if (id < 0 || technology.isEmpty() || !(programmers.containsKey(id)))
 			return false;
+		List<Programmer> tempTechProg = techProgrammers.getOrDefault(technology, new ArrayList<>());
+		techProgrammers.put(technology, tempTechProg);
 		return programmers.get(id).getTechnologies().add(technology);
 	}
 
@@ -62,6 +70,8 @@ public class ProgrammersMaps implements IProgrammer {
 	public boolean removeTechnology(int id, String technology) {
 		if (id < 0 || technology.isEmpty() || !(programmers.containsKey(id)))
 			return false;
+		Programmer programmer = getProgrammerData(id);
+		programmer.getTechnologies().remove(technology);
 		return programmers.get(id).getTechnologies().remove(technology);
 	}
 
@@ -69,14 +79,15 @@ public class ProgrammersMaps implements IProgrammer {
 	public List<Programmer> getProgrammersWithTechnology(String technology) {
 		if (technology.isEmpty())
 			return null;
-		List<Programmer> res = new ArrayList<>();
-		for (Programmer programmer : programmers.values()) {
-			Set<String> tech = programmer.getTechnologies();
-			if (!tech.isEmpty() && tech.contains(technology)) {
-				res.add(programmer);
-			}
-		}
-		return res;
+		List<Programmer> res = techProgrammers.get(technology);
+//		List<Programmer> res = new ArrayList<>();
+//		for (Programmer programmer : programmers.values()) {
+//			Set<String> tech = programmer.getTechnologies();
+//			if (!tech.isEmpty() && tech.contains(technology)) {
+//				res.add(programmer);
+//			}
+//		}
+		return res != null ? res : new ArrayList<>();
 	}
 
 	@Override
@@ -84,20 +95,28 @@ public class ProgrammersMaps implements IProgrammer {
 		if (salaryFrom < 0 || salaryFrom > salaryTo)
 			return null;
 		List<Programmer> res = new ArrayList<>();
-		for (Programmer programmer : programmers.values()) {
-			int salary = programmer.getSalary();
-			if (salary >= salaryFrom && salary <= salaryTo) {
-				res.add(programmer);
-			}
+		for (List<Programmer> list : salaryProgrammers.subMap(salaryFrom, salaryTo).values()) {
+			res.addAll(list);
 		}
-		return res;
+//		for (Programmer programmer : programmers.values()) {
+//			int salary = programmer.getSalary();
+//			if (salary >= salaryFrom && salary <= salaryTo) {
+//				res.add(programmer);
+//			}
+//		}
+		return new ArrayList<>(res);
 	}
 
 	@Override
 	public boolean updateSalary(int id, int salary) {
 		if (id < 0 || salary <= 0 || !(programmers.containsKey(id)))
 			return false;
-		programmers.get(id).setSalary(salary);
+		Programmer programmer = getProgrammerData(id);
+		salaryProgrammers.get(programmer.getSalary()).remove(programmer);
+		programmer.setSalary(salary);
+		List<Programmer> tempSalProg = salaryProgrammers.getOrDefault(salary, new ArrayList<>());
+		salaryProgrammers.put(salary, tempSalProg);
+//		programmers.get(id).setSalary(salary);
 		return true;
 	}
 
