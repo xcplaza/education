e2cf82b43ecd3bdaf21105823724d20f8f29a1a5
xcplaza <xcplaza@gmail.com>
2023-07-15 14:34:12 +0300

    Update Weekday.java

--- 41_CW_Enum/src/Weekday.java
+++ 41_CW_Enum/src/Weekday.java
@@ -2,7 +2,7 @@
 public enum Weekday {
 	SUN, MON, TUE, WED, THU, FRI, SAT;
 
-//	свойтво - переход по дня
+//	свойтство - переход по дням
 	public Weekday plusDays(int days) {
 		Weekday[] array = values();
 		int index = ordinal();
