7b3b65a397f9d0d7ac8191fd477eb4e996ac5177
xcplaza <xcplaza@gmail.com>
2023-07-18 12:19:43 +0300

    Create Company.java

---
+++ 46_CW_StreamAPI/src/Company.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Company {
+	String name;
+	List<String> employees;
+
+	public Company(String name, List<String> employees) {
+		super();
+		this.name = name;
+		this.employees = employees;
+	}
+
+}
