a308185c14da1125a5e1d9b9098eab6e0ef47791
xcplaza <xcplaza@gmail.com>
2023-07-18 14:20:18 +0300

    Create Employeer.java

---
+++ 46_CW_StreamAPI/src/practice/Employeer.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class Employeer {
+	String name;
+	String company;
+	int salary;
+
+	public Employeer(String name, String company, int salary) {
+		super();
+		this.name = name;
+		this.company = company;
+		this.salary = salary;
+	}
+
+}
