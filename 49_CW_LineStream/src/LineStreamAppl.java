b126aa30f27f7e9da576f6e2245fd71cf39eedfd
xcplaza <xcplaza@gmail.com>
2023-07-23 13:26:27 +0300

    Update LineStreamAppl.java

--- 49_CW_LineStream/src/LineStreamAppl.java
+++ 49_CW_LineStream/src/LineStreamAppl.java
@@ -1,9 +1,17 @@
+import java.io.FileReader;
+import java.io.FileWriter;
 
 public class LineStreamAppl {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try (FileReader input = new FileReader("file.txt"); FileWriter output = new FileWriter("dest.txt");) {
+			int c = 0;
+			while ((c = input.read()) > 0) {
+				output.write(c);
+			}
+		} catch (Exception e) {
 
+		}
 	}
 
 }
