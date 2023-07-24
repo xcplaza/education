ab5c6bf212459b12d9d6cb5b6d2c6ef11a65d5ca
xcplaza <xcplaza@gmail.com>
2023-07-24 10:45:55 +0300

    Update Game.java

--- 49_CW_LineStream/src/game/Game.java
+++ 49_CW_LineStream/src/game/Game.java
@@ -3,18 +3,34 @@ package game;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Game {
+public class Game
+{
 	ArrayList<Character> randomNumber = new ArrayList<>();
-
-	public Game() {
-		new Random().ints(0, 10).distinct().limit(4).mapToObj(num -> (char) ('0' + num)).forEach(randomNumber::add);
+	
+	public Game()
+	{
+		new Random().ints(0, 10).distinct().limit(4).mapToObj(num -> (char)('0'+num))
+			.forEach(randomNumber::add);
+		System.out.println(randomNumber);
 	}
-
-	public Integer[] getMoveResult(String move) {
+	
+	public Integer[] getMoveResult(String move)
+	{
 		char[] temp = move.toCharArray();
-
-		return null;
-
+		int limit = temp.length < 4 ? temp.length : 4;
+		Integer bulls = 0;
+		Integer cows = 0;
+		for(int i=0; i<limit; i++)
+		{
+			int index = randomNumber.indexOf(temp[i]);
+			if(index >= 0)
+			{
+				if(index == i)
+					bulls++;
+				else
+					cows++;
+			}
+		}
+		return new Integer[] {bulls, cows};
 	}
-
 }
