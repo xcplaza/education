76471e297579f0802b36ca489391bc3e756b1753
xcplaza <xcplaza@gmail.com>
2023-07-24 10:46:00 +0300

    Update Move.java

--- 49_CW_LineStream/src/game/Move.java
+++ 49_CW_LineStream/src/game/Move.java
@@ -1,17 +1,21 @@
 package game;
 
-public class Move {
+public class Move
+{
 	public Integer bullsCows[];
 	public String guessNumber;
 
-	public Move(Integer[] bullsCows, String guessNumber) {
+	public Move(Integer[] bullsCows, String guessNumber)
+	{
 		super();
 		this.bullsCows = bullsCows;
 		this.guessNumber = guessNumber;
 	}
-
+	
 	@Override
-	public String toString() {
-		return String.format("guess number:%s, bulls:%d, cows:%d", guessNumber, bullsCows[0], bullsCows[1]);
+	public String toString()
+	{
+		return String.format("guess number:%s, bulls:%d, cows:%d", guessNumber, 
+				bullsCows[0], bullsCows[1]);
 	}
 }
