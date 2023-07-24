bc08f7ac3225c75c6c515233a2e377bd03eb345b
xcplaza <xcplaza@gmail.com>
2023-07-24 10:55:27 +0300

    Update DataStreamAppl.java

--- 50_CW_ObjectOrientedStream/src/DataStreamAppl.java
+++ 50_CW_ObjectOrientedStream/src/DataStreamAppl.java
@@ -1,9 +1,19 @@
+import java.io.*;
 
 public class DataStreamAppl {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		double[] prices = { 19.99, 9.99, 20., 3.99, 100 };
+		int[] units = { 12, 8, 13, 29, 2 };
+		String[] goods = { "milk", "bread", "juice", "benzin", "meet" };
 
+		DataOutputStream out = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("invoice.data")));
+		for (int i = 0; i < prices.length; i++) {
+			out.writeDouble(prices[i]);
+			out.writeInt(units[i]);
+			out.writeUTF(goods[i]);
+		}
+		out.close();
 	}
 
 }
