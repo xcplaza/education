8a2bf59f92f0d43dd65ade0f5fc3731918c41631
xcplaza <xcplaza@gmail.com>
2023-07-29 12:41:06 +0300

    Update ILibrary.java

--- Library_Sprint1/src/telran/library/model/ILibrary.java
+++ Library_Sprint1/src/telran/library/model/ILibrary.java
@@ -1,6 +1,9 @@
 package telran.library.model;
 
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
+
 import telran.library.entities.Book;
 import telran.library.entities.BooksReturnCode;
 import telran.library.entities.Reader;
@@ -8,7 +11,7 @@ import telran.library.entities.Reader;
 public interface ILibrary extends Serializable {
 	BooksReturnCode addBookItem(Book book);
 	BooksReturnCode addReader(Reader reader);
-	Reader getReader(int readerId);
-	Book getBookItem(long isbn);
 	BooksReturnCode addBookExemplars(long isbn, int amount);
+	Reader getReader(int reader);
+	Book getBookItem(long isbn);
 }
