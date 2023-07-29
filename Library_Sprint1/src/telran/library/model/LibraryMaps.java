1ce9694cbe758d15af5da0cc97e9c037aca775ba
xcplaza <xcplaza@gmail.com>
2023-07-29 12:41:08 +0300

    Update LibraryMaps.java

--- Library_Sprint1/src/telran/library/model/LibraryMaps.java
+++ Library_Sprint1/src/telran/library/model/LibraryMaps.java
@@ -1,66 +1,80 @@
 package telran.library.model;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import telran.library.entities.Book;
+import telran.library.entities.BooksReturnCode;
+import telran.library.entities.Reader;
+import telran.utils.Persistable;
+
+import java.io.*;
 import java.util.HashMap;
-import telran.library.entities.*;
-import telran.util.Persistable;
 
 @SuppressWarnings("serial")
-public class LibraryMaps extends AbstractLibrary implements Persistable {
+public class LibraryMaps extends AbstractLibrary implements Persistable
+{
 	HashMap<Long, Book> books = new HashMap<>();
 	HashMap<Integer, Reader> readers = new HashMap<>();
 
 	@Override
-	public BooksReturnCode addBookItem(Book book) {
-		if (book.getPicPeriod() < this.minPickPeriod) {
-			return BooksReturnCode.PICK_PERIOD_LASS_MIN;
-		}
-		if (book.getPicPeriod() > this.maxPickPeriod) {
-			return BooksReturnCode.PICK_PERIOD_CREATOR_MAX;
-		}
-		return books.putIfAbsent(book.getIsbn(), book) == null ? BooksReturnCode.OK : BooksReturnCode.BOOK_ITEM_EXIST;
+	public BooksReturnCode addBookItem(Book book)
+	{
+		if (book.getPickPeriod() < minPickPeriod)
+			return BooksReturnCode.PICK_PERIOD_LESS_MIN;
+		if (book.getPickPeriod() > maxPickPeriod)
+			return BooksReturnCode.PICK_PERIOD_GREATER_MAX;
+		return books.putIfAbsent(book.getIsbn(), book) == null ? BooksReturnCode.OK : 
+			BooksReturnCode.BOOK_ITEM_EXISTS;
 	}
 
 	@Override
-	public BooksReturnCode addReader(Reader reader) {
+	public BooksReturnCode addReader(Reader reader)
+	{
 		return readers.putIfAbsent(reader.getReaderId(), reader) == null ? BooksReturnCode.OK
-				: BooksReturnCode.READER_EXIST;
+				: BooksReturnCode.READER_EXISTS;
 	}
 
 	@Override
-	public Reader getReader(int readerId) {
-		return readers.get(readerId);
+	public BooksReturnCode addBookExemplars(long isbn, int amount)
+	{
+		Book book = getBookItem(isbn);
+		if (book == null)
+			return BooksReturnCode.NO_BOOK_ITEM;
+		book.setAmount(book.getAmount() + amount);
+		return BooksReturnCode.OK;
+	}
+	
+	@Override
+	public Reader getReader(int reader)
+	{
+		return readers.get(reader);
 	}
 
 	@Override
-	public Book getBookItem(long isbn) {
+	public Book getBookItem(long isbn)
+	{
 		return books.get(isbn);
 	}
 
 	@Override
-	public BooksReturnCode addBookExemplars(long isbn, int amount) {
-		if (!books.containsKey(isbn))
-			return BooksReturnCode.NO_BOOK_ITEM;
-		books.get(isbn).setAmount(books.get(isbn).getAmount() + amount);
-		return BooksReturnCode.OK;
-	}
-
-	@Override
-	public void save(String fileName) {
-		try (ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(fileName))) {
+	public void save(String fileName)
+	{
+		try (ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(fileName)))
+		{
 			output.writeObject(this);
-		} catch (Exception e) {
-			System.out.println("Error in method save " + e.getMessage());
+		} 
+		catch (Exception e)
+		{
+			System.out.println(e.getMessage());
 		}
 	}
 
-	public static ILibrary restoreFromFile(String fileName) {
-		try (ObjectInputStream input = new ObjectInputStream(new FileInputStream(fileName))) {
+	public static ILibrary restoreFromFile(String fileName)
+	{
+		try (ObjectInputStream input = new ObjectInputStream(new FileInputStream(fileName)))
+		{
 			return (LibraryMaps) input.readObject();
-		} catch (Exception e) {
+		} 
+		catch (Exception e)
+		{
 			System.out.println(e.getMessage());
 			return new LibraryMaps();
 		}
