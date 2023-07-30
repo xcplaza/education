536b58546e4592437ca10a577027f6b74dfc2d42
xcplaza <xcplaza@gmail.com>
2023-07-30 11:21:58 +0300

    Create IRentCompany.java

---
+++ RentCompany_Sprint3/src/telran/cars/model/IRentCompany.java
@@ -0,0 +1,26 @@
+package telran.cars.model;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
+import telran.cars.dto.*;
+
+public interface IRentCompany extends Serializable {
+	int getGasPrice();
+	void setGasPrice(int price);
+	int getFinePercent();
+	void setFinePercent(int percent);
+
+	CarsRetunCode addModel(Model model);
+	Model getModel(String modelName);
+	CarsRetunCode addCar(Car car);
+	Car getCar(String regNumber);
+	CarsRetunCode addDriver(Driver driver);
+	Driver getDriver(long licenseId);
+
+//	sprint2
+	CarsRetunCode rentCar(String regNumber, long licenseId, LocalDate rentDate, int rentDays);
+	List<Car> getDriverCars(long licenseId);
+	List<Driver> getCarDrivers(String regNumString);
+	List<Car> getModelCars(String modelName);
+	List<RentRecord> getRentRecordsAtDates(LocalDate fromDate, LocalDate toDate);
+}
