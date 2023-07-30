e8a0b2bcfb1e12144e63917cc1196a5ea78df2d4
xcplaza <xcplaza@gmail.com>
2023-07-30 11:22:00 +0300

    Create RentCompanyEmbeded.java

---
+++ RentCompany_Sprint3/src/telran/cars/model/RentCompanyEmbeded.java
@@ -0,0 +1,159 @@
+package telran.cars.model;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.TreeMap;
+
+import telran.util.Persistable;
+import telran.cars.dto.*;
+
+@SuppressWarnings("serial")
+public class RentCompanyEmbeded extends AbstractRentCompany implements Persistable {
+	HashMap<String, Car> cars = new HashMap<>();
+	HashMap<Long, Driver> drivers = new HashMap<>();
+	HashMap<String, Model> models = new HashMap<>();
+//	sprint2
+	HashMap<String, List<Car>> modelCars = new HashMap<>();
+	HashMap<Long, List<RentRecord>> driverRecords = new HashMap<>();
+	HashMap<String, List<RentRecord>> carRecords = new HashMap<>();
+	TreeMap<LocalDate, List<RentRecord>> records = new TreeMap<>();
+
+	@Override
+	public CarsRetunCode addModel(Model model) {
+		return models.putIfAbsent(model.getModelName(), model) == null ? CarsRetunCode.OK : CarsRetunCode.MODEL_EXISTS;
+	}
+
+	@Override
+	public Model getModel(String modelName) {
+		return models.get(modelName);
+	}
+
+	@Override
+	public CarsRetunCode addCar(Car car) {
+		if (!models.containsKey(car.getModelName()))
+			return CarsRetunCode.NO_MODEL;
+		boolean res = cars.putIfAbsent(car.getRegNumber(), car) == null;
+		if (!res) {
+			return CarsRetunCode.CAR_EXISTS;
+		}
+		addModelCars(car);
+		return CarsRetunCode.OK;
+	}
+
+	private void addModelCars(Car car) {
+		String modelName = car.getModelName();
+		List<Car> listCar = modelCars.getOrDefault(modelName, new ArrayList<>());
+		listCar.add(car);
+		modelCars.putIfAbsent(modelName, listCar);
+	}
+
+	@Override
+	public Car getCar(String regNumber) {
+		return cars.get(regNumber);
+	}
+
+	@Override
+	public CarsRetunCode addDriver(Driver driver) {
+		return drivers.putIfAbsent(driver.getLicenseId(), driver) == null ? CarsRetunCode.OK
+				: CarsRetunCode.DRIVER_EXISTS;
+	}
+
+	@Override
+	public Driver getDriver(long licenseId) {
+		return drivers.get(licenseId);
+	}
+
+	@Override
+	public void save(String fileName) {
+		try (ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(fileName))) {
+			output.writeObject(this);
+		} catch (Exception e) {
+			System.out.println("Error in method save " + e.getMessage());
+		}
+	}
+
+	public static IRentCompany restoreFromFile(String fileName) {
+		try (ObjectInputStream input = new ObjectInputStream(new FileInputStream(fileName))) {
+			return (IRentCompany) input.readObject();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return new RentCompanyEmbeded();
+		}
+	}
+
+	@Override
+	public CarsRetunCode rentCar(String regNumber, long licenseId, LocalDate rentDate, int rentDays) {
+		Car car = getCar(regNumber);
+		if (car == null) {
+			return CarsRetunCode.NO_CAR;
+		}
+		if (car.isFlRemoved()) {
+			return CarsRetunCode.CAR_REMOVED;
+		}
+		if (car.isInUse()) {
+			return CarsRetunCode.CAR_IN_USE;
+		}
+		if (!drivers.containsKey(licenseId)) {
+			return CarsRetunCode.NO_DRIVER;
+		}
+		RentRecord record = new RentRecord(regNumber, licenseId, rentDate, rentDays);
+		addToCarRecord(record);
+		addToDriverRecords(record);
+		addToRecords(record);
+		car.setInUse(true);
+		return CarsRetunCode.OK;
+	}
+
+	private void addToRecords(RentRecord record) {
+		String regNumber = record.getRegNumber();
+		List<RentRecord> listRecords = carRecords.getOrDefault(regNumber, new ArrayList<>());
+		listRecords.add(record);
+		carRecords.putIfAbsent(regNumber, listRecords);
+	}
+
+	private void addToDriverRecords(RentRecord record) {
+		long licenseId = record.getLicenseId();
+		List<RentRecord> listRecords = driverRecords.getOrDefault(licenseId, new ArrayList<>());
+		listRecords.add(record);
+		driverRecords.putIfAbsent(licenseId, listRecords);
+	}
+
+	private void addToCarRecord(RentRecord record) {
+		LocalDate rentDate = record.getRentDate();
+		List<RentRecord> listRecords = records.getOrDefault(rentDate, new ArrayList<>());
+		listRecords.add(record);
+		records.putIfAbsent(rentDate, listRecords);
+	}
+
+	@Override
+	public List<Car> getDriverCars(long licenseId) {
+		List<RentRecord> listRecords = driverRecords.getOrDefault(licenseId, new ArrayList<>());
+		return listRecords.stream().map(r -> getCar(r.getRegNumber())).distinct().toList();
+	}
+
+	@Override
+	public List<Driver> getCarDrivers(String regNumString) {
+		List<RentRecord> listRecords = carRecords.getOrDefault(regNumString, new ArrayList<>());
+		return listRecords.stream().map(d -> getDriver(d.getLicenseId())).distinct().toList();
+	}
+
+	@Override
+	public List<Car> getModelCars(String modelName) {
+		List<Car> carList = modelCars.getOrDefault(modelName, new ArrayList<>());
+		return carList.stream().filter(c -> !c.isFlRemoved() && !c.isInUse()).toList();
+	}
+
+	@Override
+	public List<RentRecord> getRentRecordsAtDates(LocalDate fromDate, LocalDate toDate) {
+		Collection<List<RentRecord>> collRecords = records.subMap(fromDate, toDate).values();
+		return collRecords.stream().flatMap(l -> l.stream()).toList();
+	}
+
+}
