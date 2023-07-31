92d4cdd28472ff45d9b9011169bd3480710674ed
xcplaza <xcplaza@gmail.com>
2023-07-31 16:53:40 +0300

    Update AbstractRentCompany.java

--- RentCompany_Sprint3/src/telran/cars/model/AbstractRentCompany.java
+++ RentCompany_Sprint3/src/telran/cars/model/AbstractRentCompany.java
@@ -4,29 +4,27 @@ package telran.cars.model;
 public abstract class AbstractRentCompany implements IRentCompany {
 	protected int finePercent = 15;
 	protected int gasPrice = 10;
-
+	
+	@Override
 	public int getFinePercent() {
 		return finePercent;
 	}
-
 	@Override
 	public void setFinePercent(int finePercent) {
 		this.finePercent = finePercent;
 	}
-
+	
 	@Override
 	public int getGasPrice() {
 		return gasPrice;
 	}
-
 	@Override
 	public void setGasPrice(int gasPrice) {
 		this.gasPrice = gasPrice;
 	}
-
 	@Override
 	public String toString() {
 		return "AbstractRentCompany [finePercent=" + finePercent + ", gasPrice=" + gasPrice + "]";
-	}
+	}		
 
 }
