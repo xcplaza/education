c36119200037c7c33ffe724bd19ba478aa812646
xcplaza <xcplaza@gmail.com>
2023-07-31 16:53:42 +0300

    Update IRentCompany.java

--- RentCompany_Sprint3/src/telran/cars/model/IRentCompany.java
+++ RentCompany_Sprint3/src/telran/cars/model/IRentCompany.java
@@ -1,31 +1,45 @@
 package telran.cars.model;
+
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.List;
+
 import telran.cars.dto.*;
 
 public interface IRentCompany extends Serializable {
 	int getGasPrice();
+
 	void setGasPrice(int price);
+
 	int getFinePercent();
+
 	void setFinePercent(int percent);
 
-	CarsRetunCode addModel(Model model);
+	CarsReturnCode addModel(Model model);
+
 	Model getModel(String modelName);
-	CarsRetunCode addCar(Car car);
+
+	CarsReturnCode addCar(Car car);
+
 	Car getCar(String regNumber);
-	CarsRetunCode addDriver(Driver driver);
+
+	CarsReturnCode addDriver(Driver driver);
+
 	Driver getDriver(long licenseId);
 
-//	sprint2
-	CarsRetunCode rentCar(String regNumber, long licenseId, LocalDate rentDate, int rentDays);
+	// Sprint2
+	CarsReturnCode rentCar(String regNumber, long licenseId, LocalDate rentDate, int rentDays);
+
 	List<Car> getDriverCars(long licenseId);
-	List<Driver> getCarDrivers(String regNumString);
+
+	List<Driver> getCarDrivers(String regNumber);
+
 	List<Car> getModelCars(String modelName);
+
 	List<RentRecord> getRentRecordsAtDates(LocalDate fromDate, LocalDate toDate);
-	
-//	sprint3
+	//Sprint3
 	RemovedCarData removeCar(String regNumber);
 	List<RemovedCarData> removeModel(String modelName);
-	RemovedCarData returnCar(String regNumber, long licenseId, LocalDate returnDate, int damages, int tankPersent);
+	RemovedCarData returnCar(String regNumber, long licenseId, LocalDate returnDate, int damages, int tankPercent);
+	
 }
