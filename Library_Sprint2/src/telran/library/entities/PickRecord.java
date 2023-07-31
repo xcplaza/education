57a4b2bc536502d9c84ca252e7bbde8b14a7b892
xcplaza <xcplaza@gmail.com>
2023-07-31 16:47:16 +0300

    Update PickRecord.java

--- Library_Sprint2/src/telran/library/entities/PickRecord.java
+++ Library_Sprint2/src/telran/library/entities/PickRecord.java
@@ -4,59 +4,71 @@ import java.io.Serializable;
 import java.time.LocalDate;
 
 @SuppressWarnings("serial")
-public class PickRecord implements Serializable {
-	private long isbn;
-	private int readerId;
-	private LocalDate pickDate;
-	private LocalDate returnDate;
-	private int delayDays;
+public class PickRecord implements Serializable
+{
+	long isbn;
+	int readerId;
+	LocalDate pickDate;
+	LocalDate returnDate;
+	int delayDays;
 
-	public PickRecord() {
-	}
-
-	public PickRecord(long isbn, int readerId, LocalDate pickDate) {
+	public PickRecord(long isbn, int readerId, LocalDate pickDate)
+	{
 		super();
 		this.isbn = isbn;
 		this.readerId = readerId;
 		this.pickDate = pickDate;
 	}
 
-	public LocalDate getReturnDate() {
+	public PickRecord()
+	{
+	}
+	
+	public LocalDate getReturnDate()
+	{
 		return returnDate;
 	}
 
-	public void setReturnDate(LocalDate returnDate) {
+	public void setReturnDate(LocalDate returnDate)
+	{
 		this.returnDate = returnDate;
 	}
 
-	public int getDelayDays() {
+	public int getDelayDays()
+	{
 		return delayDays;
 	}
 
-	public void setDelayDays(int delayDays) {
+	public void setDelayDays(int delayDays)
+	{
 		this.delayDays = delayDays;
 	}
 
-	public long getIsbn() {
+	public long getIsbn()
+	{
 		return isbn;
 	}
 
-	public int getReaderId() {
+	public int getReaderId()
+	{
 		return readerId;
 	}
 
-	public LocalDate getPickDate() {
+	public LocalDate getPickDate()
+	{
 		return pickDate;
 	}
 
 	@Override
-	public String toString() {
+	public String toString()
+	{
 		return "PickRecord [isbn=" + isbn + ", readerId=" + readerId + ", pickDate=" + pickDate + ", returnDate="
 				+ returnDate + ", delayDays=" + delayDays + "]";
 	}
 
 	@Override
-	public int hashCode() {
+	public int hashCode()
+	{
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + delayDays;
@@ -68,29 +80,33 @@ public class PickRecord implements Serializable {
 	}
 
 	@Override
-	public boolean equals(Object obj) {
+	public boolean equals(Object obj)
+	{
 		if (this == obj)
 			return true;
-		if (!(obj instanceof PickRecord))
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
 			return false;
 		PickRecord other = (PickRecord) obj;
 		if (delayDays != other.delayDays)
 			return false;
 		if (isbn != other.isbn)
 			return false;
-		if (pickDate == null) {
+		if (pickDate == null)
+		{
 			if (other.pickDate != null)
 				return false;
 		} else if (!pickDate.equals(other.pickDate))
 			return false;
 		if (readerId != other.readerId)
 			return false;
-		if (returnDate == null) {
+		if (returnDate == null)
+		{
 			if (other.returnDate != null)
 				return false;
 		} else if (!returnDate.equals(other.returnDate))
 			return false;
 		return true;
 	}
-
 }
