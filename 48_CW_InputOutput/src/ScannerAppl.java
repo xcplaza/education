baf4382f2fa4df575f395fa3475b6056cd39b729
xcplaza <xcplaza@gmail.com>
2023-08-01 09:52:23 +0300

    Update ScannerAppl.java

--- 48_CW_InputOutput/src/ScannerAppl.java
+++ 48_CW_InputOutput/src/ScannerAppl.java
@@ -4,22 +4,23 @@ import java.util.Scanner;
 public class ScannerAppl {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String str;
+		try (Scanner scan = new Scanner(System.in)) {
+			String str;
 //		System.out.println("Enter your name: ");
 //		str = scan.nextLine();
 //		System.out.println("Welcome to Israel, " + str);
 
-		ArrayList<String> names = new ArrayList<>();
-		while (true) {
-			System.out.println("Enter name text or exit for quit");
-			String res = scan.nextLine();
-			if (res.equalsIgnoreCase("exit"))
-				break;
+			ArrayList<String> names = new ArrayList<>();
+			while (true) {
+				System.out.println("Enter name text or exit for quit");
+				String res = scan.nextLine();
+				if (res.equalsIgnoreCase("exit"))
+					break;
 //			System.out.println("Your text is \"" + res + "\"");
-			names.add(res);
+				names.add(res);
+			}
+			System.out.println(names);
 		}
-		System.out.println(names);
 	}
 
 }
