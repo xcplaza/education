a4d27358aa50c3f96e207a7fb333a4af17ee5e58
xcplaza <xcplaza@gmail.com>
2023-08-01 09:46:39 +0300

    Update ProgrammersMaps.java

--- miniProjectProgrammer/src/telran/employees/service/ProgrammersMaps.java
+++ miniProjectProgrammer/src/telran/employees/service/ProgrammersMaps.java
@@ -1,17 +1,10 @@
 package telran.employees.service;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeMap;
-import java.util.Map.Entry;
-
 import telran.employees.dto.Programmer;
 
 public class ProgrammersMaps implements IProgrammer {
