e50da14ff5433d5405b965feec22e48b77430a73
xcplaza <xcplaza@gmail.com>
2023-08-01 09:46:35 +0300

    Update CalcEnergy.java

--- CalcEnergy/src/CalcEnergy.java
+++ CalcEnergy/src/CalcEnergy.java
@@ -1,8 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Enumeration;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.function.BinaryOperator;
