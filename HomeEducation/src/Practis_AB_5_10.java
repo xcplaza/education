4801cba0c3be49b65e55bf31c7606b9ee92119f2
xcplaza <xcplaza@gmail.com>
2023-08-01 09:52:34 +0300

    Update Practis_AB_5_10.java

--- HomeEducation/src/Practis_AB_5_10.java
+++ HomeEducation/src/Practis_AB_5_10.java
@@ -3,9 +3,8 @@ import java.util.Scanner;
 public class Practis_AB_5_10 {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		int a = scan.nextInt();
+		try (Scanner scan = new Scanner(System.in)) {
+		}
 		
 
 	}
