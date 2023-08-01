768d615de1be2156b5fdd59c35c8537b96057c76
xcplaza <xcplaza@gmail.com>
2023-08-01 09:52:19 +0300

    Update Calculator.java

--- 48_CW_InputOutput/src/calculator/Calculator.java
+++ 48_CW_InputOutput/src/calculator/Calculator.java
@@ -16,47 +16,36 @@ public class Calculator {
 	}
 
 	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		while (true) {
-			System.out.println("Enter first openrad or exit for quit");
-			String line = s.nextLine();
-			if (line.equalsIgnoreCase("exit"))
-				break;
-			int op1 = Integer.parseInt(line);
+		try (Scanner s = new Scanner(System.in)) {
+			while (true) {
+				System.out.println("Enter first openrad or exit for quit");
+				String line = s.nextLine();
+				if (line.equalsIgnoreCase("exit"))
+					break;
+				int op1 = Integer.parseInt(line);
 
-			System.out.println("Enter operator [+, -, *, /] or exit for quit");
-			line = s.nextLine();
-			if (line.equalsIgnoreCase("exit"))
-				break;
-			String operator = line;
+				System.out.println("Enter operator [+, -, *, /] or exit for quit");
+				line = s.nextLine();
+				if (line.equalsIgnoreCase("exit"))
+					break;
+				String operator = line;
 
-			System.out.println("Enter second openrad or exit for quit");
-			line = s.nextLine();
-			if (line.equalsIgnoreCase("exit"))
-				break;
-			int op2 = Integer.parseInt(line);
+				System.out.println("Enter second openrad or exit for quit");
+				line = s.nextLine();
+				if (line.equalsIgnoreCase("exit"))
+					break;
+				int op2 = Integer.parseInt(line);
 
-			System.out.println("res = " + computeOne(op1, op2, operator));
+				System.out.println("res = " + computeOne(op1, op2, operator));
+			}
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
 	private static Integer computeOne(Integer res, Integer operand, String operator) {
 		return mapOperations.get(operator).apply(res, operand);
-	}
-
-	// разделитель - число и пробел
-	private static String[] getOperators(String str) {
-		return str.split("[ \\d]+");
-	}
-
-	private static String[] getOperands(String str) {
-		return str.trim().split("\\D+");
-	}
-
-	// вычисляем цифры и арифметику
-	private static boolean isArithmeticExpression(String expression) {
-		String pattern = "\\s*\\d+(\\s*[*/+-]\\s*\\d+)+\\s*";
-		return expression.matches(pattern);
 	}
 
 }
