2acb9643f04224218792481cf7fd794af4190cd8
xcplaza <xcplaza@gmail.com>
2023-08-01 09:52:25 +0300

    Update BufferedFile.java

--- 49_CW_LineStream/src/BufferedFile.java
+++ 49_CW_LineStream/src/BufferedFile.java
@@ -4,18 +4,18 @@ public class BufferedFile {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader input = new BufferedReader(new FileReader("file.txt"));
-		BufferedWriter output = new BufferedWriter(new FileWriter("file1.txt"));
-
-		while (true) {
-			String res = input.readLine();
-			if (res == null) {
-				break;
+		try (BufferedWriter output = new BufferedWriter(new FileWriter("file1.txt"))) {
+			while (true) {
+				String res = input.readLine();
+				if (res == null) {
+					break;
+				}
+				output.write(res + "\n");
 			}
-			output.write(res + "\n");
-		}
-		input.close();
+			input.close();
 //		output.close();
-		output.flush();
+			output.flush();
+		}
 
 	}
 }
