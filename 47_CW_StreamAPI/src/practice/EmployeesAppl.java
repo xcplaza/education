043a93250cfe9a7c099bb64086d6b9da57a46a30
xcplaza <xcplaza@gmail.com>
2023-08-01 09:52:17 +0300

    Update EmployeesAppl.java

--- 47_CW_StreamAPI/src/practice/EmployeesAppl.java
+++ 47_CW_StreamAPI/src/practice/EmployeesAppl.java
@@ -5,10 +5,8 @@ import java.util.Arrays;
 import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class EmployeesAppl {
 
