c2c5399153a121547ffdc619f1bbb710387b9d1b
xcplaza <xcplaza@gmail.com>
2023-08-01 09:52:33 +0300

    Update Practic_Build.java

--- HomeEducation/src/Practic_Build.java
+++ HomeEducation/src/Practic_Build.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 
 public class Practic_Build {
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+		try (Scanner scan = new Scanner(System.in)) {
+			int floorCount = scan.nextInt();
 
-		int floorCount = scan.nextInt();
-
-		if (floorCount >= 1 && floorCount <= 4) {
-			System.out.println("малоэтажный дом");
-		} else if (floorCount >= 5 && floorCount <= 8) {
-			System.out.println("Среднеэтажный дом");
-		} else if (floorCount >= 9) {
-			System.out.println("Многоэтажный дом");
-		} else {
-			System.out.println("Wrong numbers floor");
+			if (floorCount >= 1 && floorCount <= 4) {
+				System.out.println("малоэтажный дом");
+			} else if (floorCount >= 5 && floorCount <= 8) {
+				System.out.println("Среднеэтажный дом");
+			} else if (floorCount >= 9) {
+				System.out.println("Многоэтажный дом");
+			} else {
+				System.out.println("Wrong numbers floor");
+			}
 		}
 	}
 
