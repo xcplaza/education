c770309f58e5266471141284f31ec39f4ad6b76c
xcplaza <xcplaza@gmail.com>
2023-08-02 18:59:04 +0300

    Create RemoveCarItem.java

---
+++ RentCompany_Sprint5/src/telran/cars/cli/manager/RemoveCarItem.java
@@ -0,0 +1,23 @@
+package telran.cars.cli.manager;
+
+import telran.cars.cli.RentCompanyItem;
+import telran.cars.model.IRentCompany;
+import telran.view.InputOutput;
+
+public class RemoveCarItem extends RentCompanyItem {
+
+	public RemoveCarItem(InputOutput inOut, IRentCompany company) {
+		super(inOut, company);
+	}
+
+	@Override
+	public String displayName() {
+		return "Remove car";
+	}
+
+	@Override
+	public void perform() {
+		inOut.outputLine(company.removeCar(getRegNumberIfNotExist()));
+	}
+
+}
