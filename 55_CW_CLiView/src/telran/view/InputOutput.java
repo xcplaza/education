bc80f69d3c58062a9d52a55242dabea3d559ae12
xcplaza <xcplaza@gmail.com>
2023-08-02 11:53:10 +0300

    Update InputOutput.java

--- 55_CW_CLiView/src/telran/view/InputOutput.java
+++ 55_CW_CLiView/src/telran/view/InputOutput.java
@@ -1,5 +1,8 @@
 package telran.view;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.function.Function;
 
 public interface InputOutput {
@@ -8,7 +11,7 @@ public interface InputOutput {
 	String inputString(String prompt);
 
 	default void outputLine(Object obj) {
-		output(obj.toString());
+		output(obj);
 	}
 
 	default <R> R inputObject(String prompt, String errorPromt, Function<String, R> mapper) {
@@ -39,6 +42,43 @@ public interface InputOutput {
 			try {
 				Integer res = Integer.parseInt(s);
 				return res >= min && res <= max ? res : null;
+			} catch (Exception e) {
+				return null;
+			}
+		});
+	}
+
+	default String InputString(String prompt, List<String> options) {
+		return inputObject(prompt, "String is not in options", s -> options.contains(s) ? s : null);
+	}
+
+	default Double inputDouble(String prompt) {
+		return inputObject(prompt, "It's not a double number", s -> {
+			try {
+				Double number = Double.parseDouble(s);
+				return number;
+			} catch (Exception e) {
+				return null;
+			}
+		});
+	}
+
+	default Long inputLong(String prompt) {
+		return inputObject(prompt, "It's not a long number", s -> {
+			try {
+				Long number = Long.parseLong(s);
+				return number;
+			} catch (Exception e) {
+				return null;
+			}
+		});
+	}
+
+	default LocalDate inputDate(String prompt, String format) {
+		return inputObject(prompt, "Wrong date " + format, s -> {
+			try {
+				DateTimeFormatter dtf = DateTimeFormatter.ofPattern(format);
+				return LocalDate.parse(s, dtf);
 			} catch (Exception e) {
 				return null;
 			}
