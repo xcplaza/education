d118ee91128c7e36f1c90e57099b9c6f12803b2e
xcplaza <xcplaza@gmail.com>
2023-08-02 15:29:11 +0300

    Update AddModelItem.java

--- RentCompany_Sprint5/src/telran/cars/cli/manager/AddModelItem.java
+++ RentCompany_Sprint5/src/telran/cars/cli/manager/AddModelItem.java
@@ -1,5 +1,46 @@
 package telran.cars.cli.manager;
 
-public class AddModelItem {
-	
+import telran.cars.cli.RentCompanyItem;
+import telran.cars.dto.Model;
+import telran.cars.model.IRentCompany;
+import telran.view.InputOutput;
+
+public class AddModelItem extends RentCompanyItem {
+	private static final int MIN_TANK = 35;
+	private static final int MAX_TANK = 100;
+	private static final int MIN_PRICE = 50;
+	private static final int MAX_PRICE = 1000;
+
+	public AddModelItem(InputOutput inOut, IRentCompany company) {
+		super(inOut, company);
+	}
+
+	@Override
+	public String displayName() {
+		return "Add car model";
+	}
+
+	@Override
+	public void perform() {
+		String modelName = inOut.inputString("Enter model name");
+		if (modelName == null)
+			return;
+		Integer gasTank = inOut.inputInteger(
+				String.format("Enter gas tank volume from range [%d - %d]", MIN_TANK, MAX_TANK), MIN_TANK, MAX_TANK);
+		if (gasTank == null)
+			return;
+		String companyName = inOut.inputString("Enter company name");
+		if (companyName == null)
+			return;
+		String country = inOut.inputString("Enter country");
+		if (country == null)
+			return;
+		Integer priceDay = inOut.inputInteger(
+				String.format("Enter price day from range [%d - %d]", MIN_PRICE, MAX_PRICE), MIN_PRICE, MAX_PRICE);
+		if (priceDay == null)
+			return;
+		Model model = new Model(modelName, gasTank, companyName, country, priceDay);
+		inOut.outputLine(company.addModel(model));
+	}
+
 }
