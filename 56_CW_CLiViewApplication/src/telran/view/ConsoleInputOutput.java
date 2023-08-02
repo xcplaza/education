fd9d3eeca2c4cc78e590b75079ae003d51bb77af
xcplaza <xcplaza@gmail.com>
2023-08-02 13:31:52 +0300

    Create ConsoleInputOutput.java

---
+++ 56_CW_CLiViewApplication/src/telran/view/ConsoleInputOutput.java
@@ -0,0 +1,20 @@
+package telran.view;
+
+import java.util.Scanner;
+
+public class ConsoleInputOutput implements InputOutput {
+	Scanner scanner = new Scanner(System.in);
+
+	@Override
+	public void output(Object obj) {
+		System.out.println(obj);
+	}
+
+	@Override
+	public String inputString(String prompt) {
+		System.out.println(prompt + " or cancel for exit");
+		String text = scanner.nextLine();
+		return text.equalsIgnoreCase("cancel") ? null : text;
+	}
+
+}
