d697fb70012ed8018c7cfc3e18a4effaded80258
xcplaza <xcplaza@gmail.com>
2023-08-05 12:22:51 +0300

    Update IRentCompany.java

--- RentCompany_Sprint5/src/telran/cars/model/IRentCompany.java
+++ RentCompany_Sprint5/src/telran/cars/model/IRentCompany.java
@@ -6,65 +6,37 @@ import java.util.List;
 
 import telran.cars.dto.*;
 
-public interface IRentCompany extends Serializable
-{
-//	Sprint1
+public interface IRentCompany extends Serializable {
 	int getGasPrice();
 	void setGasPrice(int price);
 	int getFinePercent();
-	void setFinePercent(int finePercent);
-	
-	CarsReturnCode addCar(Car car);
-	CarsReturnCode addDriver(Driver driver);
+	void setFinePercent(int percent);
+
 	CarsReturnCode addModel(Model model);
-	
-	Car getCar(String regNumber);
 	Model getModel(String modelName);
+	CarsReturnCode addCar(Car car);
+	Car getCar(String regNumber);
+	CarsReturnCode addDriver(Driver driver);
 	Driver getDriver(long licenseId);
-	
-//	Sprint 2
+
+	// Sprint2
 	CarsReturnCode rentCar(String regNumber, long licenseId, LocalDate rentDate, int rentDays);
-//	list cars, which was rented by driver with this licenseId
-	List<Car> getCarsByDriver(long licenseId);
-//	list drivers, which rented car with this regNumber
-	List<Driver> getDriversByCar(String regNumber);
-//	list cars of model with this modelName
-	List<Car> getCarsByModel(String modelName);
-//	list records, in which rent date in period from date to date
+	List<Car> getDriverCars(long licenseId);
+	List<Driver> getCarDrivers(String regNumber);
+	List<Car> getModelCars(String modelName);
 	List<RentRecord> getRentRecordsAtDates(LocalDate fromDate, LocalDate toDate);
 	
-//	Sprint 3
+	//Sprint3
 	RemovedCarData removeCar(String regNumber);
-	List<RemovedCarData> removeCarsOfModel(String modelName);
+	List<RemovedCarData> removeModel(String modelName);
 	RemovedCarData returnCar(String regNumber, long licenseId, LocalDate returnDate, int damages, int tankPercent);
-//	RemoverCarData = {car = Car, List<RentRecord> = null}
 	
-//	Sprint 4
+	//Sprint4
 	List<String> getMostPopularCarModels(LocalDate fromDate, LocalDate toDate, int fromAge, int toAge);
 	List<String> getMostProfitableCarModels(LocalDate fromDate, LocalDate toDate);
 	List<Driver> getMostActiveDriver();
 	
-//	Sprint5
+//	sprint5
 	List<String> getModelNames();
+	List<Long> getLicenseDriver();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
