49b2756784f0f40eb04a01434da0281c000abcb8
xcplaza <xcplaza@gmail.com>
2023-08-05 12:21:27 +0300

    Update AddCarItem.java

--- RentCompany_Sprint5/src/telran/cars/cli/manager/AddCarItem.java
+++ RentCompany_Sprint5/src/telran/cars/cli/manager/AddCarItem.java
@@ -7,38 +7,31 @@ import telran.cars.dto.Car;
 import telran.cars.model.IRentCompany;
 import telran.view.InputOutput;
 
-public class AddCarItem extends RentCompanyItem
-{
-	public AddCarItem(InputOutput inOut, IRentCompany company)
-	{
+public class AddCarItem extends RentCompanyItem {
+
+	public AddCarItem(InputOutput inOut, IRentCompany company) {
 		super(inOut, company);
 	}
 
 	@Override
-	public String displayedName()
-	{
+	public String displayName() {
 		return "Add car";
 	}
 
 	@Override
-	public void perform()
-	{
-		String regNumber = getRegNumberIfNotExists();
-		if(regNumber == null)
+	public void perform() {
+		String regNumber = getRegNumberIfNotExist();
+		if (regNumber == null)
 			return;
-		
 		String color = inOut.inputString("Enter color");
-		if(color == null)
+		if (color == null)
 			return;
-		
 		List<String> models = company.getModelNames();
-		String modelName = inOut.inputString("Enter model from " + models, models);
-		if(modelName == null)
+		String modelName = inOut.InputString("Enter model from" + models, models);
+		if (modelName == null)
 			return;
-		
 		Car car = new Car(regNumber, color, modelName);
 		inOut.outputLine(company.addCar(car));
 	}
-
 
 }
