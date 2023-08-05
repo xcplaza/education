7aa2e44b573483cb306aa89a95481e95a8cc2368
xcplaza <xcplaza@gmail.com>
2023-08-05 19:34:48 +0300

    Update RemovedBookData.java

--- Library_Sprint5/src/telran/library/entities/RemovedBookData.java
+++ Library_Sprint5/src/telran/library/entities/RemovedBookData.java
@@ -2,30 +2,31 @@ package telran.library.entities;
 
 import java.util.*;
 
-public class RemovedBookData
-{
+public class RemovedBookData {
 	Book book;
 	List<PickRecord> records;// list of the records related to the book
 	// if book isn't actually deleted the records is null
 
-	public RemovedBookData()
-	{
+	public RemovedBookData() {
 	}
-	
-	public RemovedBookData(Book book, List<PickRecord> records)
-	{
+
+	public RemovedBookData(Book book, List<PickRecord> records) {
 		super();
 		this.book = book;
 		this.records = records;
 	}
 
-	public Book getBook()
-	{
+	public Book getBook() {
 		return book;
 	}
 
-	public List<PickRecord> getRecords()
-	{
+	public List<PickRecord> getRecords() {
 		return records;
 	}
+
+	@Override
+	public String toString() {
+		return "RemovedBookData [book=" + book + ", records=" + records + "]";
+	}
+
 }
