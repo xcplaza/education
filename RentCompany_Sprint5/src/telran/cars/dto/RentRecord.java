82b87b125214b704fc68a1c1f30709a649b69339
xcplaza <xcplaza@gmail.com>
2023-08-05 12:22:34 +0300

    Update RentRecord.java

--- RentCompany_Sprint5/src/telran/cars/dto/RentRecord.java
+++ RentCompany_Sprint5/src/telran/cars/dto/RentRecord.java
@@ -2,10 +2,10 @@ package telran.cars.dto;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.Objects;
 
 @SuppressWarnings("serial")
-public class RentRecord implements Serializable
-{
+public class RentRecord implements Serializable {
 	private String regNumber;
 	private long licenseId;
 	private LocalDate rentDate;
@@ -15,146 +15,89 @@ public class RentRecord implements Serializable
 	private int tankPercent;
 	private double cost;
 	
-	public RentRecord()
-	{
-		// TODO Auto-generated constructor stub
-	}
+	public RentRecord() {}
 
-	public RentRecord(String regNumber, long licenseId, LocalDate rentDate, int rentDays)
-	{
+	public RentRecord(String regNumber, long licenseId, LocalDate rentDate, int rentDays) {
 		super();
 		this.regNumber = regNumber;
 		this.licenseId = licenseId;
 		this.rentDate = rentDate;
 		this.rentDays = rentDays;
 	}
-	
-	public LocalDate getReturnDate()
-	{
+
+	public LocalDate getReturnDate() {
 		return returnDate;
 	}
 
-	public void setReturnDate(LocalDate returnDate)
-	{
+	public void setReturnDate(LocalDate returnDate) {
 		this.returnDate = returnDate;
 	}
 
-	public int getDamages()
-	{
+	public int getDamages() {
 		return damages;
 	}
 
-	public void setDamages(int damages)
-	{
+	public void setDamages(int damages) {
 		this.damages = damages;
 	}
 
-	public int getTankPercent()
-	{
+	public int getTankPercent() {
 		return tankPercent;
 	}
 
-	public void setTankPercent(int tankPercent)
-	{
+	public void setTankPercent(int tankPercent) {
 		this.tankPercent = tankPercent;
 	}
 
-	public double getCost()
-	{
+	public double getCost() {
 		return cost;
 	}
 
-	public void setCost(double cost)
-	{
+	public void setCost(double cost) {
 		this.cost = cost;
 	}
 
-	public String getRegNumber()
-	{
+	public String getRegNumber() {
 		return regNumber;
 	}
 
-	public long getLicenseId()
-	{
+	public long getLicenseId() {
 		return licenseId;
 	}
 
-	public LocalDate getRentDate()
-	{
+	public LocalDate getRentDate() {
 		return rentDate;
 	}
 
-	public int getRentDays()
-	{
+	public int getRentDays() {
 		return rentDays;
 	}
 
 	@Override
-	public int hashCode()
-	{
-		final int prime = 31;
-		int result = 1;
-		long temp;
-		temp = Double.doubleToLongBits(cost);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		result = prime * result + damages;
-		result = prime * result + (int) (licenseId ^ (licenseId >>> 32));
-		result = prime * result + ((regNumber == null) ? 0 : regNumber.hashCode());
-		result = prime * result + ((rentDate == null) ? 0 : rentDate.hashCode());
-		result = prime * result + rentDays;
-		result = prime * result + ((returnDate == null) ? 0 : returnDate.hashCode());
-		result = prime * result + tankPercent;
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj)
-	{
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		RentRecord other = (RentRecord) obj;
-		if (Double.doubleToLongBits(cost) != Double.doubleToLongBits(other.cost))
-			return false;
-		if (damages != other.damages)
-			return false;
-		if (licenseId != other.licenseId)
-			return false;
-		if (regNumber == null)
-		{
-			if (other.regNumber != null)
-				return false;
-		} else if (!regNumber.equals(other.regNumber))
-			return false;
-		if (rentDate == null)
-		{
-			if (other.rentDate != null)
-				return false;
-		} else if (!rentDate.equals(other.rentDate))
-			return false;
-		if (rentDays != other.rentDays)
-			return false;
-		if (returnDate == null)
-		{
-			if (other.returnDate != null)
-				return false;
-		} else if (!returnDate.equals(other.returnDate))
-			return false;
-		if (tankPercent != other.tankPercent)
-			return false;
-		return true;
-	}
-
-	@Override
-	public String toString()
-	{
+	public String toString() {
 		return "RentRecord [regNumber=" + regNumber + ", licenseId=" + licenseId + ", rentDate=" + rentDate
 				+ ", returnDate=" + returnDate + ", rentDays=" + rentDays + ", damages=" + damages + ", tankPercent="
 				+ tankPercent + ", cost=" + cost + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cost, damages, licenseId, regNumber, rentDate, rentDays, returnDate, tankPercent);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof RentRecord))
+			return false;
+		RentRecord other = (RentRecord) obj;
+		return Double.doubleToLongBits(cost) == Double.doubleToLongBits(other.cost) && damages == other.damages
+				&& licenseId == other.licenseId && Objects.equals(regNumber, other.regNumber)
+				&& Objects.equals(rentDate, other.rentDate) && rentDays == other.rentDays
+				&& Objects.equals(returnDate, other.returnDate) && tankPercent == other.tankPercent;
+	}
 	
 	
+
 }
