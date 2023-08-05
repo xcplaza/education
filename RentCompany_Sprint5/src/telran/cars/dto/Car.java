cdd4d4152ea7416e196c59336977392a8364ee12
xcplaza <xcplaza@gmail.com>
2023-08-05 12:23:03 +0300

    Update Car.java

--- RentCompany_Sprint5/src/telran/cars/dto/Car.java
+++ RentCompany_Sprint5/src/telran/cars/dto/Car.java
@@ -1,108 +1,80 @@
 package telran.cars.dto;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @SuppressWarnings("serial")
-public class Car implements Serializable
-{
+public class Car implements Serializable {
 	private String regNumber;
 	private String color;
-	private State state;
+	private State state = State.EXCELLENT;
 	private String modelName;
 	private boolean inUse;
 	private boolean flRemoved;
 	
-	public Car(String regNumber, String color, String modelName)
-	{
+	public Car() {}
+
+	public Car(String regNumber, String color, String modelName) {
 		super();
 		this.regNumber = regNumber;
 		this.color = color;
 		this.modelName = modelName;
-		state = State.EXCELLENT;
-	}
-	
-	public Car()
-	{
-		// TODO Auto-generated constructor stub
 	}
 
-	public State getState()
-	{
+	public State getState() {
 		return state;
 	}
 
-	public void setState(State state)
-	{
+	public void setState(State state) {
 		this.state = state;
 	}
 
-	public boolean isUse()
-	{
+	public boolean isInUse() {
 		return inUse;
 	}
 
-	public void setUse(boolean isUse)
-	{
-		this.inUse = isUse;
+	public void setInUse(boolean inUse) {
+		this.inUse = inUse;
 	}
 
-	public boolean isFlRemoved()
-	{
+	public boolean isFlRemoved() {
 		return flRemoved;
 	}
 
-	public void setFlRemoved(boolean flRemoved)
-	{
+	public void setFlRemoved(boolean flRemoved) {
 		this.flRemoved = flRemoved;
 	}
 
-	public String getRegNumber()
-	{
+	public String getRegNumber() {
 		return regNumber;
 	}
 
-	public String getColor()
-	{
+	public String getColor() {
 		return color;
 	}
 
-	public String getModelName()
-	{
+	public String getModelName() {
 		return modelName;
 	}
-	
+
 	@Override
-	public int hashCode()
-	{
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((regNumber == null) ? 0 : regNumber.hashCode());
-		return result;
+	public String toString() {
+		return "Car [regNumber=" + regNumber + ", color=" + color + ", state=" + state + ", modelName=" + modelName
+				+ ", inUse=" + inUse + ", flRemoved=" + flRemoved + "]";
 	}
 
 	@Override
-	public boolean equals(Object obj)
-	{
+	public int hashCode() {
+		return Objects.hash(regNumber);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
+		if (!(obj instanceof Car))
 			return false;
 		Car other = (Car) obj;
-		if (regNumber == null)
-		{
-			if (other.regNumber != null)
-				return false;
-		} else if (!regNumber.equals(other.regNumber))
-			return false;
-		return true;
-	}
-
-	@Override
-	public String toString()
-	{
-		return "Car [regNumber=" + regNumber + ", color=" + color + ", state=" + state + ", modelName=" + modelName
-				+ ", isUse=" + inUse + ", flRemoved=" + flRemoved + "]";
-	}
+		return Objects.equals(regNumber, other.regNumber);
+	}	
 }
