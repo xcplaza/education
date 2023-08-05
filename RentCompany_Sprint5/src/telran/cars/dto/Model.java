7dd8ba761d35c8098ab6c598de0a02f69e06b53f
xcplaza <xcplaza@gmail.com>
2023-08-05 12:22:54 +0300

    Update Model.java

--- RentCompany_Sprint5/src/telran/cars/dto/Model.java
+++ RentCompany_Sprint5/src/telran/cars/dto/Model.java
@@ -1,60 +1,19 @@
 package telran.cars.dto;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @SuppressWarnings("serial")
-public class Model implements Serializable
-{
+public class Model implements Serializable {
 	private String modelName;
 	private int gasTank;
 	private String company;
 	private String country;
 	private int priceDay;
 	
-	public int getPriceDay()
-	{
-		return priceDay;
-	}
+	public Model() {}
 
-	@Override
-	public String toString()
-	{
-		return "Model [modelName=" + modelName + ", gasTank=" + gasTank + ", company=" + company + ", country="
-				+ country + ", priceDay=" + priceDay + "]";
-	}
-
-	public void setPriceDay(int priceDay)
-	{
-		this.priceDay = priceDay;
-	}
-
-	public String getModelName()
-	{
-		return modelName;
-	}
-
-	public int getGasTank()
-	{
-		return gasTank;
-	}
-
-	public String getCompany()
-	{
-		return company;
-	}
-
-	public String getCountry()
-	{
-		return country;
-	}
-
-	public Model()
-	{
-		// TODO Auto-generated constructor stub
-	}
-
-	public Model(String modelName, int gasTank, String company, String country, int priceDay)
-	{
+	public Model(String modelName, int gasTank, String company, String country, int priceDay) {
 		super();
 		this.modelName = modelName;
 		this.gasTank = gasTank;
@@ -63,33 +22,48 @@ public class Model implements Serializable
 		this.priceDay = priceDay;
 	}
 
-	@Override
-	public int hashCode()
-	{
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((modelName == null) ? 0 : modelName.hashCode());
-		return result;
+	public int getPriceDay() {
+		return priceDay;
+	}
+
+	public void setPriceDay(int priceDay) {
+		this.priceDay = priceDay;
+	}
+
+	public String getModelName() {
+		return modelName;
+	}
+
+	public int getGasTank() {
+		return gasTank;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public String getCountry() {
+		return country;
 	}
 
 	@Override
-	public boolean equals(Object obj)
-	{
+	public String toString() {
+		return "Model [modelName=" + modelName + ", gasTank=" + gasTank + ", company=" + company + ", country="
+				+ country + ", priceDay=" + priceDay + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(modelName);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
+		if (!(obj instanceof Model))
 			return false;
 		Model other = (Model) obj;
-		if (modelName == null)
-		{
-			if (other.modelName != null)
-				return false;
-		} else if (!modelName.equals(other.modelName))
-			return false;
-		return true;
-	}
-	
-	
+		return Objects.equals(modelName, other.modelName);
+	}		
 }
