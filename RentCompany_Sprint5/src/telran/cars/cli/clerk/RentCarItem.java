6f509def296fe13f697d091b5cca61e5f9ff731d
xcplaza <xcplaza@gmail.com>
2023-08-05 12:21:16 +0300

    Update RentCarItem.java

--- RentCompany_Sprint5/src/telran/cars/cli/clerk/RentCarItem.java
+++ RentCompany_Sprint5/src/telran/cars/cli/clerk/RentCarItem.java
@@ -6,49 +6,35 @@ import telran.cars.cli.RentCompanyItem;
 import telran.cars.model.IRentCompany;
 import telran.view.InputOutput;
 
-public class RentCarItem extends RentCompanyItem
-{
+public class RentCarItem extends RentCompanyItem {
+	private static final int MIN_DAYS = 1;
+	private static final int MAX_DAYS = 365;
 
-	public RentCarItem(InputOutput inputOutput, IRentCompany company)
-	{
-		super(inputOutput, company);
+	public RentCarItem(InputOutput inOut, IRentCompany company) {
+		super(inOut, company);
 	}
 
-	private static final int MIN_RENT_DAYS = 1;
-	private static final int MAX_RENT_DAYS = 30;
-
 	@Override
-	public String displayedName()
-	{
+	public String displayName() {
 		return "Rent car";
 	}
 
 	@Override
-	public void perform()
-	{
-		String regNumber = getRegNumberIfExists();
-		if(regNumber == null)
-			return;
+	public void perform() {
 		
-		if(company.getCar(regNumber).isUse())
-		{
-			inOut.outputLine("Car is already in use");
+		String regNumber = getRegNumberIfNotExist();
+		if (regNumber == null)
 			return;
-		}
-		
-		Long licenseId = getLicenseIdIfExists();
-		if(licenseId == null)
+		Long licenseId = inOut.inputLong("Enter licence ID");
+		if (licenseId == null)
 			return;
-		
-		LocalDate rentDate = inOut.inputDate("Enter rent date using format " + format, format);
-		if(rentDate == null)
+		LocalDate rentDate = LocalDate.now();
+		if (rentDate == null)
 			return;
-		
-		Integer rentDays = inOut.inputInteger(String.format("Enter rent days from range [%d-%d]", 
-				MIN_RENT_DAYS, MAX_RENT_DAYS), MIN_RENT_DAYS, MAX_RENT_DAYS);
-		if(rentDays == null)
+		Integer rentDays = inOut.inputInteger(String.format("Enter rent day from range [%d - %d]", MIN_DAYS, MAX_DAYS),
+				MIN_DAYS, MAX_DAYS);
+		if (rentDays == null)
 			return;
-		
 		inOut.outputLine(company.rentCar(regNumber, licenseId, rentDate, rentDays));
 	}
 
