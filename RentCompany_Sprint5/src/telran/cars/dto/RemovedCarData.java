fc0abeb34d26d6b9d01f6fe8ed0470c5689f7194
xcplaza <xcplaza@gmail.com>
2023-08-05 12:22:50 +0300

    Update RemovedCarData.java

--- RentCompany_Sprint5/src/telran/cars/dto/RemovedCarData.java
+++ RentCompany_Sprint5/src/telran/cars/dto/RemovedCarData.java
@@ -4,36 +4,29 @@ import java.io.Serializable;
 import java.util.List;
 
 @SuppressWarnings("serial")
-public class RemovedCarData implements Serializable
-{
+public class RemovedCarData implements Serializable {
 	private Car car;
 	private List<RentRecord> removedRecords;
 	
-	public RemovedCarData()
-	{
-		// TODO Auto-generated constructor stub
-	}
+	public RemovedCarData() {}
 
-	public RemovedCarData(Car car, List<RentRecord> removedRecords)
-	{
+	public RemovedCarData(Car car, List<RentRecord> removedRecords) {
 		super();
 		this.car = car;
 		this.removedRecords = removedRecords;
 	}
 
-	public Car getCar()
-	{
+	public Car getCar() {
 		return car;
 	}
 
-	public List<RentRecord> getRemovedRecords()
-	{
+	public List<RentRecord> getRemovedRecords() {
 		return removedRecords;
 	}
 
 	@Override
-	public String toString()
-	{
+	public String toString() {
 		return "RemovedCarData [car=" + car + ", removedRecords=" + removedRecords + "]";
-	}
+	}	
+
 }
