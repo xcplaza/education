5c937f3418d0f64e24e5533c58cc8e7f16dcfeeb
xcplaza <xcplaza@gmail.com>
2023-08-06 13:43:27 +0300

    Create CalculatorServer.java

---
+++ CalculatorClient/src/CalculatorServer.java
@@ -0,0 +1,9 @@
+import telran.server.ProtocolJava;
+import telran.server.ServerJava;
+
+public class CalculatorServer extends ServerJava {
+
+	public CalculatorServer(ProtocolJava protocol, int port) throws Exception {
+		super(protocol, port);
+	}
+}
