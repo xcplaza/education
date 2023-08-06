353616b2ccea936f3b155029c52e51d3ea89348f
xcplaza <xcplaza@gmail.com>
2023-08-06 13:43:29 +0300

    Create CalculatorTestAppl.java

---
+++ CalculatorClient/src/CalculatorTestAppl.java
@@ -0,0 +1,23 @@
+
+import client.CalculatorClient;
+import telran.view.*;
+
+import telran.view.ConsoleInputOutput;
+import telran.view.InputOutput;
+import telran.view.Item;
+
+public class CalculatorTestAppl {
+	static CalculatorClient cc;
+	static InputOutput inOut;
+
+	public static void main(String[] args) throws Exception {
+		cc =  new CalculatorClient("localhost", 2000);
+		inOut = new ConsoleInputOutput();
+		Item[] items = { 
+				new CalculatorItem(inOut, cc),
+				new CloseExitItem(inOut, cc)
+		};
+		Menu menu = new Menu(items, inOut);
+		menu.runMenu();
+	}
+}
