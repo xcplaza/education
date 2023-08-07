323d940fd24319c1f4d4ff89497045a02bfb88c6
xcplaza <xcplaza@gmail.com>
2023-08-07 08:59:57 +0300

    Update EmployeeServerAppl.java

--- 58_CW_EmployeeServer/src/telran/employees/EmployeeServerAppl.java
+++ 58_CW_EmployeeServer/src/telran/employees/EmployeeServerAppl.java
@@ -1,18 +1,19 @@
 package telran.employees;
 
-import telran.employees.net.EmployessTcpProtocol;
+import telran.employees.net.EmployeesTcpProtocol;
 import telran.employees.service.EmployeesMaps;
-import telran.employees.service.IEMployees;
+import telran.employees.service.IEmployees;
 import telran.server.ProtocolJava;
 import telran.server.ServerJava;
 
 public class EmployeeServerAppl {
 
 	public static void main(String[] args) throws Exception {
-		IEMployees service = new EmployeesMaps();
-		ProtocolJava protocol = new EmployessTcpProtocol(service);
+		IEmployees service = new EmployeesMaps();
+		ProtocolJava protocol = new EmployeesTcpProtocol(service);
 		ServerJava server = new ServerJava(protocol, 2000);
 		server.run();
+
 	}
 
 }
