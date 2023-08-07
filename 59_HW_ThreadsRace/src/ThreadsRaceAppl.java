cbe2269ccb28d8ee8fdc64f92485df798fb53638
xcplaza <xcplaza@gmail.com>
2023-08-07 18:57:40 +0300

    Create ThreadsRaceAppl.java

---
+++ 59_HW_ThreadsRace/src/ThreadsRaceAppl.java
@@ -0,0 +1,20 @@
+
+import telran.view.ConsoleInputOutput;
+import telran.view.ExitItem;
+import telran.view.InputOutput;
+import telran.view.Item;
+import telran.view.Menu;
+
+public class ThreadsRaceAppl {
+
+	public static void main(String[] args) {
+		InputOutput inOut = new ConsoleInputOutput();
+		Item[] items = { 
+				new ThreadsRaceItem(inOut, null),
+				new ExitItem(),
+		};
+		Menu menu = new Menu(items, inOut);
+		menu.runMenu();
+	}
+
+}
