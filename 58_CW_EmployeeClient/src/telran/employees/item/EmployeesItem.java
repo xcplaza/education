ba463e1d41335602fb3a5a1155c3f1b361e6b4f9
xcplaza <xcplaza@gmail.com>
2023-08-07 15:07:06 +0300

    Update EmployeesItem.java

--- 58_CW_EmployeeClient/src/telran/employees/item/EmployeesItem.java
+++ 58_CW_EmployeeClient/src/telran/employees/item/EmployeesItem.java
@@ -7,12 +7,11 @@ import telran.view.Item;
 public abstract class EmployeesItem implements Item {
 	protected InputOutput inputOutput;
 	protected IEmployees employees;
+
 	public EmployeesItem(InputOutput inputOutput, IEmployees employees) {
 		super();
 		this.inputOutput = inputOutput;
 		this.employees = employees;
 	}
-	
-	
 
 }
