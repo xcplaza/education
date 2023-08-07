9e52800a2550144b3ff6ac0d733c4eeb12c8967e
xcplaza <xcplaza@gmail.com>
2023-08-07 08:59:45 +0300

    Update IEMployees.java

--- 58_CW_EmployeeClient/src/telran/employees/service/IEMployees.java
+++ 58_CW_EmployeeClient/src/telran/employees/service/IEMployees.java
@@ -5,12 +5,18 @@ import java.util.List;
 import telran.employees.dto.CompanySalary;
 import telran.employees.dto.Employee;
 
-public interface IEMployees {
+public interface IEmployees {
 	boolean hireEmployee(Employee emp);
+
 	boolean fireEmployee(int id);
+
 	Employee getEmployeeData(int id);
+
 	List<Employee> getEmployeesData(int salaryFrom, int salaryTo);
+
 	List<CompanySalary> getCompaniesAvgSalary();
+
 	List<CompanySalary> getCompaniesGreaterAvgSalary();
+
 	int getAvgSalary();
 }
