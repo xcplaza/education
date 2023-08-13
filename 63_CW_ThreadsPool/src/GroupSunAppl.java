1019a1c92e8d489275d54d1c1898e2ba572b9949
xcplaza <xcplaza@gmail.com>
2023-08-13 21:51:37 +0300

    Update GroupSunAppl.java

--- 63_CW_ThreadsPool/src/GroupSunAppl.java
+++ 63_CW_ThreadsPool/src/GroupSunAppl.java
@@ -2,19 +2,38 @@ import java.util.Random;
 
 public class GroupSunAppl {
 	private static final int N_GROUPS = 1_000; // количество мешков
-	private static final int N_NUMBERS = 10_000; // количество гранат в мешке
+	private static final int N_NUMBERS = 1_000; // количество гранат в мешке
 
 	public static void main(String[] args) {
 		int[][] numbers = getRandomNumbers();
 		ExecutorServiceGroupSum service = new ExecutorServiceGroupSum(numbers);
 		GroupPerformanceTest test = new GroupPerformanceTest(service);
-
-		for (int nThreads = 1000; nThreads <= 10000; nThreads += 1000) {
-			System.out.println("nThreads " + nThreads);
-			service.setnThreads(nThreads);
-			test.runTest();
-		}
+		
+		GroupSumStream streamSrv = new GroupSumStream(numbers);
+		GroupPerformanceTest stream = new GroupPerformanceTest(streamSrv);
+		
+		GroupSumThreads threadsSrv = new GroupSumThreads(numbers);
+		GroupPerformanceTest threads = new GroupPerformanceTest(threadsSrv);
+		
+		GroupSumStreamParalels paralellsSrv = new GroupSumStreamParalels(numbers);
+		GroupPerformanceTest parallels = new GroupPerformanceTest(paralellsSrv);
+		
 		test.runTest();
+
+//		for (int nThreads = 1000; nThreads <= 10000; nThreads += 1000) {
+//			System.out.println("nThreads " + nThreads);
+//			service.setnThreads(nThreads);
+//			test.runTest();
+//		}
+
+		System.out.println("=========================");
+		stream.runTest();
+		
+		System.out.println("=========================");
+		threads.runTest();
+		
+		System.out.println("=========================");
+		parallels.runTest();
 	}
 
 	private static int[][] getRandomNumbers() {
