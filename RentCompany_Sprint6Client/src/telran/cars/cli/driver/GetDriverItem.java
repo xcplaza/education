5297fb883b4ae81d8701a6c74a58fe6317a5b297
xcplaza <xcplaza@gmail.com>
2023-08-15 14:48:11 +0300

    Create GetDriverItem.java

---
+++ RentCompany_Sprint6Client/src/telran/cars/cli/driver/GetDriverItem.java
@@ -0,0 +1,30 @@
+package telran.cars.cli.driver;
+
+import telran.cars.cli.RentCompanyItem;
+import telran.cars.model.IRentCompany;
+import telran.view.InputOutput;
+
+public class GetDriverItem extends RentCompanyItem
+{
+
+	public GetDriverItem(InputOutput inOut, IRentCompany company)
+	{
+		super(inOut, company);
+	}
+
+	@Override
+	public String displayName()
+	{
+		return "Get driver data";
+	}
+
+	@Override
+	public void perform()
+	{
+		Long id = getLicenseIdIfExists();
+		if(id == null)
+			return;
+		
+		inOut.outputLine(company.getDriver(id));
+	}
+}
