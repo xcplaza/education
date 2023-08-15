eeb96dd005e7f2bd91fc53dac04c7db6a02fa6da
xcplaza <xcplaza@gmail.com>
2023-08-15 15:29:53 +0300

    Update RentCompanyClientAppl.java

--- RentCompany_Sprint6Client/src/telran/cars/RentCompanyClientAppl.java
+++ RentCompany_Sprint6Client/src/telran/cars/RentCompanyClientAppl.java
@@ -11,6 +11,7 @@ import telran.cars.cli.manager.RemoveModelItem;
 import telran.cars.cli.statist.MostActiveDriversItem;
 import telran.cars.cli.statist.MostPopularModelsItem;
 import telran.cars.model.IRentCompany;
+import telran.cars.model.RentCompanyProxy;
 import telran.view.ConsoleInputOutput;
 import telran.view.ExitItem;
 import telran.view.InputOutput;
@@ -24,7 +25,7 @@ public class RentCompanyClientAppl {
 	static String hostname = "localhost";
 	static int port = 2000;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		company = new RentCompanyProxy(hostname, port);
 		Menu menu = new Menu(getMainMenuItems(), inOut);
 		menu.runMenu();
@@ -37,7 +38,6 @@ public class RentCompanyClientAppl {
 				new SubMenu("Manager", inOut, getManagerMenuItems()),
 				new SubMenu("Statist", inOut, getStatistMenuItems()),
 				new SubMenu("Technic", inOut, getTechMenuItems()),
-//				new SaveAndExit(inOut, company, COMPANY_FILE),
 				new ExitItem()
 		};
 		return res;
