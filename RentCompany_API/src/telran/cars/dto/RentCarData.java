955b33afc17db8603796a3c6863bb9ecb505ba93
xcplaza <xcplaza@gmail.com>
2023-08-15 14:07:26 +0300

    Create RentCarData.java

---
+++ RentCompany_API/src/telran/cars/dto/RentCarData.java
@@ -0,0 +1,56 @@
+package telran.cars.dto;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@SuppressWarnings("serial")
+public class RentCarData implements Serializable {
+	String regNumber;
+	long licenseId;
+	LocalDate rentDate;
+	int rentDays;
+
+	public RentCarData() {
+	}
+
+	public RentCarData(String regNumber, long licenseId, LocalDate rentDate, int rentDays) {
+		super();
+		this.regNumber = regNumber;
+		this.licenseId = licenseId;
+		this.rentDate = rentDate;
+		this.rentDays = rentDays;
+	}
+
+	public String getRegNumber() {
+		return regNumber;
+	}
+
+	public void setRegNumber(String regNumber) {
+		this.regNumber = regNumber;
+	}
+
+	public long getLicenseId() {
+		return licenseId;
+	}
+
+	public void setLicenseId(long licenseId) {
+		this.licenseId = licenseId;
+	}
+
+	public LocalDate getRentDate() {
+		return rentDate;
+	}
+
+	public void setRentDate(LocalDate rentDate) {
+		this.rentDate = rentDate;
+	}
+
+	public int getRentDays() {
+		return rentDays;
+	}
+
+	public void setRentDays(int rentDays) {
+		this.rentDays = rentDays;
+	}
+
+}
