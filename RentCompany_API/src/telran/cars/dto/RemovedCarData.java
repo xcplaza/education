93f71fe048655dc21d1c6c2261724a7cd0e79e05
xcplaza <xcplaza@gmail.com>
2023-08-15 14:07:21 +0300

    Create RemovedCarData.java

---
+++ RentCompany_API/src/telran/cars/dto/RemovedCarData.java
@@ -0,0 +1,32 @@
+package telran.cars.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+@SuppressWarnings("serial")
+public class RemovedCarData implements Serializable {
+	private Car car;
+	private List<RentRecord> removedRecords;
+	
+	public RemovedCarData() {}
+
+	public RemovedCarData(Car car, List<RentRecord> removedRecords) {
+		super();
+		this.car = car;
+		this.removedRecords = removedRecords;
+	}
+
+	public Car getCar() {
+		return car;
+	}
+
+	public List<RentRecord> getRemovedRecords() {
+		return removedRecords;
+	}
+
+	@Override
+	public String toString() {
+		return "RemovedCarData [car=" + car + ", removedRecords=" + removedRecords + "]";
+	}	
+
+}
