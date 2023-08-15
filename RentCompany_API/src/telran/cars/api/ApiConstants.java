8a67f6a5b35c872a20b000bf939d69f0bbad1e6c
xcplaza <xcplaza@gmail.com>
2023-08-15 14:07:36 +0300

    Create ApiConstants.java

---
+++ RentCompany_API/src/telran/cars/api/ApiConstants.java
@@ -0,0 +1,23 @@
+package telran.cars.api;
+
+public interface ApiConstants {
+	String ADD_DRIVER = "/driver/add";
+	String GET_MODEL = "/model";
+	String GET_DRIVER_CARS = "/driver/cars";
+	String GET_CAR_MODELS = "/models";
+	String GET_MODEL_CARS = "/model/cars";
+	String RENT_CAR = "/car/rent";
+	String RETURN_CAR = "/car/return";
+	String GET_CAR_DRIVERS = "/drivers/car";
+	String GET_CAR = "/car";
+	String GET_DRIVER = "/driver";
+	String ADD_CAR = "/car/add";
+	String ADD_MODEL = "/model/add";
+	String REMOVE_CAR = "/car/remove";
+	String REMOVE_MODEL = "/model/remove";
+	String GET_MOST_ACTIVE_DRIVERS = "/drivers/active";
+	String GET_MOST_POPULAR_MODELS = "/models/popular";
+	String GET_MOST_PROFITABLE_MODELS = "/models/profitable";
+	String GET_RECORDS = "/records";
+
+}
