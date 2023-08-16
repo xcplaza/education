171eaef659ed56efa22d20893c4863f0983e4477
xcplaza <xcplaza@gmail.com>
2023-08-16 09:07:40 +0300

    Update ServerJava.java

--- 65_CW_TCPJava2/src/telran/server/ServerJava.java
+++ 65_CW_TCPJava2/src/telran/server/ServerJava.java
@@ -4,7 +4,6 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ServerJava implements Runnable {
