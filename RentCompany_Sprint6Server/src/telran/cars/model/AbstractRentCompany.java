a3755221fd313bf4c1a1568305ff23c0f145f44b
xcplaza <xcplaza@gmail.com>
2023-08-16 09:35:18 +0300

    Create AbstractRentCompany.java

---
+++ RentCompany_Sprint6Server/src/telran/cars/model/AbstractRentCompany.java
@@ -0,0 +1,30 @@
+package telran.cars.model;
+
+@SuppressWarnings("serial")
+public abstract class AbstractRentCompany implements IRentCompany {
+	protected int finePercent = 15;
+	protected int gasPrice = 10;
+	
+	@Override
+	public int getFinePercent() {
+		return finePercent;
+	}
+	@Override
+	public void setFinePercent(int finePercent) {
+		this.finePercent = finePercent;
+	}
+	
+	@Override
+	public int getGasPrice() {
+		return gasPrice;
+	}
+	@Override
+	public void setGasPrice(int gasPrice) {
+		this.gasPrice = gasPrice;
+	}
+	@Override
+	public String toString() {
+		return "AbstractRentCompany [finePercent=" + finePercent + ", gasPrice=" + gasPrice + "]";
+	}		
+
+}
