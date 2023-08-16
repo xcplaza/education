2889185ac8058156e05291f687e576539dc7189c
xcplaza <xcplaza@gmail.com>
2023-08-16 12:56:07 +0300

    Update RentCompanyServerAppl.java

--- RentCompany_Sprint6Server/src/telran/cars/RentCompanyServerAppl.java
+++ RentCompany_Sprint6Server/src/telran/cars/RentCompanyServerAppl.java
@@ -1,25 +1,42 @@
 package telran.cars;
 
+import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import telran.cars.model.IRentCompany;
 import telran.cars.model.RentCompanyEmbedded;
+import telran.cars.model.RentCompanyProtocol;
 import telran.server.ProtocolJava;
 import telran.server.ServerJava;
+import telran.util.Persistable;
 
 public class RentCompanyServerAppl {
 	private static final String FILE_NAME = "compant.data";
 	private static final int PORT = 2000;
-	private static final int TIME_OUT = 10_000;
+	private static final int TIMEOUT = 10_000;
 //	private static final int N_THREADS = 10;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		IRentCompany service = RentCompanyEmbedded.restoreFromFile(FILE_NAME);
 		ProtocolJava protocol = new RentCompanyProtocol(service);
 		int threads = Runtime.getRuntime().availableProcessors();
 		ExecutorService executor = Executors.newFixedThreadPool(threads - 1);
-		ServerJava server = new ServerJava(protocol, PORT, executor, TIME_OUT);
+		ServerJava server = new ServerJava(protocol, PORT, executor, TIMEOUT);
+		executor.execute(server);
+
+		Scanner scanner = new Scanner(System.in);
+		while (true) {
+			String line = scanner.nextLine();
+			if (line.equalsIgnoreCase("exit"))
+				break;
+		}
+		((Persistable) service).save(FILE_NAME);
+		executor.shutdown();
+		server.shutdown();
+		executor.awaitTermination(TIMEOUT + 1000, TimeUnit.MILLISECONDS); // принудительный выход! (не обязательно)
+		scanner.close();
 	}
 
 }
