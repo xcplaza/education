113d7f5daa4aab98c97837f9ac9b14a7f2c162ed
xcplaza <xcplaza@gmail.com>
2023-08-16 09:07:44 +0300

    Update ReturnCarItem.java

--- RentCompany_Sprint6Client/src/telran/cars/cli/ReturnCarItem.java
+++ RentCompany_Sprint6Client/src/telran/cars/cli/ReturnCarItem.java
@@ -3,9 +3,9 @@ package telran.cars.cli;
 import java.time.LocalDate;
 import java.util.List;
 
-import telran.cars.cli.RentCompanyItem;
 import telran.cars.model.IRentCompany;
 import telran.view.InputOutput;
+
 
 public class ReturnCarItem extends RentCompanyItem {
 
