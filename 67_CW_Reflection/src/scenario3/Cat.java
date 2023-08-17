2b690fd5f9a1b974e9f6280ee6efd345c4d68818
xcplaza <xcplaza@gmail.com>
2023-08-17 15:21:49 +0300

    Create Cat.java

---
+++ 67_CW_Reflection/src/scenario3/Cat.java
@@ -0,0 +1,15 @@
+package scenario3;
+
+public class Cat {
+	public String name;
+	protected int age;
+	private boolean gender;
+
+	public Cat(String name, int age, boolean gender) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+	}
+
+}
