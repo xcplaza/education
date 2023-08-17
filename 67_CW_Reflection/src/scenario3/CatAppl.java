6bcfd8d1e020f1b2f313c6372de46bb88a2b7eeb
xcplaza <xcplaza@gmail.com>
2023-08-17 15:27:13 +0300

    Update CatAppl.java

--- 67_CW_Reflection/src/scenario3/CatAppl.java
+++ 67_CW_Reflection/src/scenario3/CatAppl.java
@@ -1,16 +1,25 @@
 package scenario3;
 
+import java.lang.reflect.Field;
 import java.util.Arrays;
 
 public class CatAppl {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Cat cat = new Cat("name", 10, false);
 		Class clazz = cat.getClass();
 
 		System.out.println(Arrays.toString(clazz.getFields()));
 		System.out.println("===========================");
 		System.out.println(Arrays.toString(clazz.getDeclaredFields()));
+
+		cat.age = 120;
+		System.out.println(cat.age);
+
+		Field fieldGenger = clazz.getDeclaredField("gender");
+		fieldGenger.setAccessible(true);
+		fieldGenger.setBoolean(cat, true);
+		System.out.println(fieldGenger.getBoolean(cat));
 	}
 
 }
