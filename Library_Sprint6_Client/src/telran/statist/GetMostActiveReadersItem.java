97608755f67ebfbe8403049ea082794cac871863
xcplaza <xcplaza@gmail.com>
2023-08-20 16:25:20 +0300

    Create GetMostActiveReadersItem.java

---
+++ Library_Sprint6_Client/src/telran/statist/GetMostActiveReadersItem.java
@@ -0,0 +1,42 @@
+package telran.statist;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import telran.books.dto.Reader;
+import telran.library.cli.LibraryItem;
+import telran.books.model.ILibrary;
+import telran.view.InputOutput;
+
+public class GetMostActiveReadersItem extends LibraryItem {
+
+	public GetMostActiveReadersItem(InputOutput inOut, ILibrary library) {
+		super(inOut, library);
+	}
+
+	@Override
+	public String displayName() {
+
+		return "Get Most Active Readers";
+	}
+
+	@Override
+	public void perform() {
+	    try {
+	        LocalDate fromDate = inOut.inputDate("Enter from Date", "yyyy-MM-dd");
+	        List<Reader> readers = library.getMostActiveReaders(fromDate, LocalDate.now());
+	        if (fromDate == null)
+	            return;
+	        if (readers == null) {
+	            inOut.outputLine("No readers to display");
+	            return;
+	        }
+	        readers.forEach(inOut::outputLine);
+	    } catch (NoSuchElementException e) {
+	        // Обработка исключения, когда ввод отсутствует или некорректен.
+	        inOut.outputLine("No readers to display");
+	    }
+	}
+
+}
