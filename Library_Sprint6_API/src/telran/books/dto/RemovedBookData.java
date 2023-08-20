50be01e0f021fa78f0ab72e20bc6f1692221024b
xcplaza <xcplaza@gmail.com>
2023-08-20 16:25:37 +0300

    Create RemovedBookData.java

---
+++ Library_Sprint6_API/src/telran/books/dto/RemovedBookData.java
@@ -0,0 +1,32 @@
+package telran.books.dto;
+
+import java.util.*;
+
+public class RemovedBookData {
+	Book book;
+	List<PickRecord> records;// list of the records related to the book
+	// if book isn't actually deleted the records is null
+
+	public RemovedBookData() {
+	}
+
+	public RemovedBookData(Book book, List<PickRecord> records) {
+		super();
+		this.book = book;
+		this.records = records;
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public List<PickRecord> getRecords() {
+		return records;
+	}
+
+	@Override
+	public String toString() {
+		return "RemovedBookData [book=" + book + ", records=" + records + "]";
+	}
+
+}
