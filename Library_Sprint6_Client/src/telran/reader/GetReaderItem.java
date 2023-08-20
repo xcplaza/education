c7d83fb8f23b009fc8ada0262db5348bad2a8c8c
xcplaza <xcplaza@gmail.com>
2023-08-20 16:25:26 +0300

    Create GetReaderItem.java

---
+++ Library_Sprint6_Client/src/telran/reader/GetReaderItem.java
@@ -0,0 +1,29 @@
+package telran.reader;
+
+import telran.books.model.ILibrary;
+import telran.library.cli.LibraryItem;
+
+import telran.view.InputOutput;
+
+public class GetReaderItem extends LibraryItem {
+
+	public GetReaderItem(InputOutput inOut, ILibrary library) {
+		super(inOut, library);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String displayName() {
+		// TODO Auto-generated method stub
+		return "Get Reader";
+	}
+
+	@Override
+	public void perform() {
+		Integer readerId = inOut.inputInteger("Enter Reader ID or cancel");
+		if (readerId == null)
+			return;
+		inOut.outputLine(library.getReader(readerId));
+	}
+
+}
