f56a9d01d6ea1c407ca6423e79e4a03ba4157d96
xcplaza <xcplaza@gmail.com>
2023-08-20 16:25:01 +0300

    Create PickBookItem.java

---
+++ Library_Sprint6_Client/src/telran/librarian/PickBookItem.java
@@ -0,0 +1,31 @@
+package telran.librarian;
+
+import java.time.LocalDate;
+
+import telran.library.cli.LibraryItem;
+import telran.books.model.ILibrary;
+import telran.view.InputOutput;
+
+public class PickBookItem extends LibraryItem {
+
+	public PickBookItem(InputOutput inOut, ILibrary library) {
+		super(inOut, library);
+	}
+
+	@Override
+	public String displayName() {
+		return "Pick book";
+	}
+
+	@Override
+	public void perform() {
+		Long isbn = inOut.inputLong("Enter isbn book");
+		if (isbn == null || isbn < 0)
+			return;
+		Integer readerId = inOut.inputInteger("Enter readerId");
+		if (readerId == null || readerId < 0)
+			return;
+		inOut.outputLine(library.pickBook(isbn, readerId, LocalDate.now()));
+	}
+
+}
