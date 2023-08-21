6e6256f4ee25cf8e684564b055af2f3eb1178fb4
xcplaza <xcplaza@gmail.com>
2023-08-21 13:08:37 +0300

    Create A2.java

---
+++ 68_CW_Annotation/src/A2.java
@@ -0,0 +1,14 @@
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(RUNTIME)
+@Target(FIELD)
+public @interface A2 {
+	String value();
+	int number() default 0;
+	boolean unique() default true;
+
+}
