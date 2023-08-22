5f0297edc90fdfdf7b87d99cfceb3524b07c03a3
xcplaza <xcplaza@gmail.com>
2023-08-22 09:44:26 +0300

    Update Table.java

--- 68_HW_Annotation/src/Table.java
+++ 68_HW_Annotation/src/Table.java
@@ -1,12 +1,11 @@
-import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
-
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RUNTIME)
-@Target(TYPE)
-public @interface Table {
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Table 
+{
 	String name();
-
 }
