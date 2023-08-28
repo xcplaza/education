03e1c43e6321bb54228b43222c7aaee3c05c52ef
xcplaza <xcplaza@gmail.com>
2023-08-28 16:09:47 +0300

    Create A.java

---
+++ maven_first/src/main/java/telran/A.java
@@ -0,0 +1,22 @@
+package telran;
+
+public class A {
+	int A;
+
+	public A() {
+	}
+
+	public A(int a) {
+		super();
+		A = a;
+	}
+
+	public int getA() {
+		return A;
+	}
+
+	public void setA(int a) {
+		A = a;
+	}
+
+}
