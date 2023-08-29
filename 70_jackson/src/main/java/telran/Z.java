8b2efecc7065ae633247540671e3ad73b0c73791
xcplaza <xcplaza@gmail.com>
2023-08-29 12:02:26 +0300

    Create Z.java

---
+++ 70_jackson/src/main/java/telran/Z.java
@@ -0,0 +1,13 @@
+package telran;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Z {
+	Y[] arrayY;
+
+}
