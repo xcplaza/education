d585d9fb8ab4bd40f3be9da7401defa99e2402d0
xcplaza <xcplaza@gmail.com>
2023-08-29 12:02:30 +0300

    Create Address.java

---
+++ 70_jackson/src/main/java/persons/Address.java
@@ -0,0 +1,30 @@
+package persons;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@SuppressWarnings("serial")
+public class Address implements Serializable {
+	String city;
+	String street;
+	int building;
+	int flat;
+
+	public Address(String city, String street, int building, int flat) {
+		super();
+		this.city = city;
+		this.street = street;
+		this.building = building;
+		this.flat = flat;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [city=" + city + ", street=" + street + ", building=" + building + ", flat=" + flat + "]";
+	}
+
+}
