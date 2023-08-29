ac3a7e22d28ed988962801c71990c3b4ac1824af
xcplaza <xcplaza@gmail.com>
2023-08-29 21:46:09 +0300

    Create Index.java

---
+++ 68_HW_Annotation/src/telran/Index.java
@@ -0,0 +1,12 @@
+package telran;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Index 
+{
+	boolean unique() default false;
+}
