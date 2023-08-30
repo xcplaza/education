9c2758d5048ec877aa6e0d4f3661149523ae392e
xcplaza <xcplaza@gmail.com>
2023-08-30 17:43:56 +0300

    Create Post.java

---
+++ 71_REST/src/main/java/telran/Post.java
@@ -0,0 +1,14 @@
+package telran;
+
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@ToString
+public class Post {
+	public int userId;
+	public int id;
+	public String title;
+	public String body;
+
+}
