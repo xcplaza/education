09044e69c3a993704f9d3ab4c0f3f49e1c813126
xcplaza <xcplaza@gmail.com>
2023-08-31 10:36:40 +0300

    Update RestClientAppl.java

--- 71_REST/src/main/java/telran/RestClientAppl.java
+++ 71_REST/src/main/java/telran/RestClientAppl.java
@@ -8,29 +8,35 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-public class RestClientAppl {
+public class RestClientAppl
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		String base_url = "https://jsonplaceholder.typicode.com/";
 		RestTemplate rest = new RestTemplate();
-
-//		ResponseEntity<String> response = rest.exchange(base_url + "posts/1", HttpMethod.GET, null, String.class);
-//		System.out.println(response);
-
-		ResponseEntity<List<Post>> res = rest.exchange(base_url + "posts", HttpMethod.GET, null,
-				new ParameterizedTypeReference<List<Post>>() {});
-		Post post = res.getBody().stream().max((p1, p2) -> p1.title.length() - p2.title.length()).orElse(null);
+		
+		ResponseEntity<String> response = rest.exchange(base_url+"posts/1", HttpMethod.GET, null, 
+				String.class);
+		System.out.println(response);
+//		System.out.println(response.getBody());
+//		System.out.println(response.getHeaders());
+//		System.out.println(response.getStatusCode());
+		
+		ResponseEntity<List<Post>> res = rest.exchange(base_url+"posts", HttpMethod.GET, null,
+				new ParameterizedTypeReference<List<Post>>(){});
+		
+		Post post = res.getBody().stream().max((p1, p2) -> p1.title.length() - p2.title.length())
+				.orElse(null);
 		System.out.println(post);
 		
-		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(base_url).queryParam("tId", 1).queryParam("postId", 1).queryParam("p", 1);
-		System.out.println(builder.toString());
-		
-//		https://jsonplaceholder.typicode.com/comments?tId=1&postId=1&p=1 - request params
-//		https://jsonplaceholder.typicode.com/posts/1/comments - path variable
-		
-//		Basic YWNjX2E2ZGE3MmNkMGVkMWNlNzo2MzczZTM0MTljOThjNzU1NzVmMjY3MmE5NTI4ODkzMQ==
-		
-
+		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(base_url)
+				.queryParam("tId", 1).queryParam("postId", 1).queryParam("p", 1);
+		System.out.println(builder.toUriString());
 	}
 
 }
+///https://jsonplaceholder.typicode.com/?tId=1&postId=1&p=1
+///https://jsonplaceholder.typicode.com/?tId=1&postId=1&p=1 - request params
+///https://jsonplaceholder.typicode.com/comments/11/1/1 - path variable
+//Basic YWNjXzk5ZjUyMjhlNTNkMWU4MzpiZDc4ODIyZTgwNzlhNzhkZTllYzViN2U1MDQyYmJjNg==
