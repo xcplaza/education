93d54a59b6a97b4101260a7ea6c1e1cc4e4705a8
xcplaza <xcplaza@gmail.com>
2023-08-31 15:01:29 +0300

    Create ICalculator.java

---
+++ 72_CalculatorClient/src/main/java/telran/service/ICalculator.java
@@ -0,0 +1,10 @@
+package telran.service;
+
+import java.util.Set;
+
+public interface ICalculator {
+	Set<String> getOperationCodes();
+
+	double calculate(double op1, double op2, String operation);
+
+}
