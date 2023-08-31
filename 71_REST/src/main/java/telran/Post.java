3294ea04429ccccb434e827b8b68aae7692ff3d6
xcplaza <xcplaza@gmail.com>
2023-08-31 10:36:42 +0300

    Update Post.java

--- 71_REST/src/main/java/telran/Post.java
+++ 71_REST/src/main/java/telran/Post.java
@@ -1,14 +1,16 @@
 package telran;
 
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+@AllArgsConstructor
 @NoArgsConstructor
 @ToString
-public class Post {
+public class Post
+{
 	public int userId;
 	public int id;
 	public String title;
 	public String body;
-
 }
