76870329fb904fba4852af9aab8989188973665e
xcplaza <xcplaza@gmail.com>
2023-08-31 15:01:32 +0300

    Create CalculatorClientAppl.java

---
+++ 72_CalculatorClient/src/main/java/telran/CalculatorClientAppl.java
@@ -0,0 +1,22 @@
+package telran;
+
+import telran.items.CalculationItem;
+import telran.items.DisplayOperationsItem;
+import telran.service.ICalculator;
+import telran.view.ConsoleInputOutput;
+import telran.view.ExitItem;
+import telran.view.InputOutput;
+import telran.view.Item;
+import telran.view.Menu;
+
+public class CalculatorClientAppl {
+	static InputOutput inputOutput = new ConsoleInputOutput();
+	static ICalculator calculator;
+
+	public static void main(String[] args) {
+		Item items[] = { new DisplayOperationsItem(inputOutput, calculator),
+				new CalculationItem(inputOutput, calculator), new ExitItem() };
+		Menu menu = new Menu(items, inputOutput);
+		menu.runMenu();
+	}
+}
