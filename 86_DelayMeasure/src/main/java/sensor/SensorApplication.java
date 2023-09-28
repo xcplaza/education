3e91138976269bdb029f1ea8e4e10d5b10679d8f
xcplaza <xcplaza@gmail.com>
2023-09-28 10:28:14 +0300

    create 86_DelayMeasure

---
+++ 86_DelayMeasure/src/main/java/sensor/SensorApplication.java
@@ -0,0 +1,16 @@
+package sensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class SensorApplication {
+
+    public static void main(String[] args) throws InterruptedException {
+        ConfigurableApplicationContext cac = SpringApplication.run(SensorApplication.class, args);
+        Thread.sleep(30000);
+        cac.close();
+    }
+
+}
