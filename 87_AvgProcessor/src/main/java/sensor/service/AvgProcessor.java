e2e9a7c175b5fbd5bdb6f987f92a3d975cac3d28
xcplaza <xcplaza@gmail.com>
2023-10-01 11:30:28 +0300

    update 87_AvgProcessor

--- 87_AvgProcessor/src/main/java/sensor/service/AvgProcessor.java
+++ 87_AvgProcessor/src/main/java/sensor/service/AvgProcessor.java
@@ -8,7 +8,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import sensor.dto.Sensor;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 @Service
 public class AvgProcessor {
@@ -17,20 +24,45 @@ public class AvgProcessor {
     @Autowired
     StreamBridge bridge;
 
-    @Value("${min_normal_value:100}")
-    int minNormalValue;
+    @Value("${period:10}")
+    int period;
 
-    @Value("${max_normal_value:220}")
-    int maxNormalValue;
+    HashMap<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>();
+    Instant timestamp = Instant.now();
 
     @Bean
-    public Consumer<Sensor> receiveSensorData() {
+    public Consumer<String> receiveSensorData() {
         return data -> {
-            if (data.value < minNormalValue)
-                bridge.send("lowData-out-0", data);
-            if (data.value > maxNormalValue)
-                bridge.send("highData-out-0", data);
-            bridge.send("normalData-out-0", data);
+            Sensor sensor = null;
+            try {
+                sensor = mapper.readValue(data, Sensor.class);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            addToMap(sensor.id, sensor.value);
+            if (ChronoUnit.SECONDS.between(timestamp, Instant.now()) >= period)
+                avgProcessing();
         };
+    }
+
+    private void avgProcessing() {
+        timestamp = Instant.now();
+        map.forEach((k, v) -> bridge.send("avgData-out-0", getPlayload(k, v)));
+        map.clear();
+    }
+
+    private String getPlayload(final Integer k, final List<Integer> v) {
+        Sensor sensor = new Sensor(System.currentTimeMillis(), k, (int) (v.stream().collect(Collectors.averagingInt(x -> x)) + 0.5));
+        try {
+            return mapper.writeValueAsString(v);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private void addToMap(final int id, final int value) {
+        List<Integer> list = map.getOrDefault(id, new ArrayList<>());
+        list.add(value);
+        map.putIfAbsent(id, list);
     }
 }
