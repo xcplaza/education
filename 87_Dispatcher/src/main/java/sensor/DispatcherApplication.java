ab60fe367807617633dd6c5a3a2193a65be4f120
xcplaza <xcplaza@gmail.com>
2023-10-01 10:28:21 +0300

    create 87_Dispatcher

---
+++ 87_Dispatcher/src/main/java/sensor/DispatcherApplication.java
@@ -0,0 +1,13 @@
+package sensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatcherApplication {
+
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(DispatcherApplication.class, args);
+    }
+
+}
