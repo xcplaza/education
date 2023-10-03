ed00ddcf70e4deaad20fc9d19eb3b726f6ed925d
xcplaza <xcplaza@gmail.com>
2023-10-03 21:04:53 +0300

    create

---
+++ 89_DisplayBigValue/src/main/java/sensor/DisplayBigValueApplication.java
@@ -0,0 +1,13 @@
+package sensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisplayBigValueApplication {
+
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(DisplayBigValueApplication.class, args);
+    }
+
+}
---
+++ 89_DisplaySmallValue/src/main/java/sensor/service/DisplayValue.java
@@ -0,0 +1,31 @@
+package sensor.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+import sensor.dto.Sensor;
+
+import java.util.function.Consumer;
+
+@Service
+public class DisplayValue {
+    ObjectMapper mapper = new ObjectMapper(); // function for create JSON
+
+    @Bean
+    public Consumer<String> receiveSensorData() {
+        return sensorData -> {
+            Sensor sensor = null;
+            try {
+                sensor = mapper.readValue(sensorData, Sensor.class);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            System.out.printf("Serial number: %d, Patient id: %d, value: %d\n", sensor.serNumber, sensor.id, sensor.value);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
+}
