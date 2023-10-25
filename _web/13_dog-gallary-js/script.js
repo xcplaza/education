2d7383d7ecba53d378f4bdf94b15a54cb1d684aa
xcplaza <xcplaza@gmail.com>
2023-10-25 14:44:08 +0300

    update

--- _web/13_dog-gallary-js/script.js
+++ _web/13_dog-gallary-js/script.js
@@ -2,15 +2,29 @@ const detailsImage = document.querySelector('.details-image');
 const detailsTitle = document.querySelector('.details-title');
 const detailContainer = document.querySelector('.details-container');
 const anchorElements = document.querySelectorAll('.thumbnails-anchor');
+const main = document.querySelector('main');
+const hiddenButton = document.querySelector('.hide-button');
 
 for (let a of anchorElements) {
     a.onclick = function () {
         setDetails(a);
-
     }
+}
+
+hiddenButton.onclick = function () {
+    hideDetails();
 }
 
 function setDetails(a) {
     detailsImage.src = a.getAttribute('data-details-image');
     detailsTitle.innerHTML = a.getAttribute('data-details-title');
+    showDetails();
+}
+
+function showDetails() {
+    main.classList.remove('hidden');
+}
+
+function hideDetails() {
+    main.classList.add('hidden');
 }
