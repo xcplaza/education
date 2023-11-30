ab8c01359bd34e4a76a943d3af325a350ea094c5
xcplaza <xcplaza@gmail.com>
2023-11-30 11:35:10 +0200

    add new test in AvgReducerTest

--- microservices/avg-reducer/src/test/java/telran/microservices/probes/AvgReducerTest.java
+++ microservices/avg-reducer/src/test/java/telran/microservices/probes/AvgReducerTest.java
@@ -2,9 +2,11 @@ package telran.microservices.probes;
 
 import java.util.*;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dto.Probe;
 import entities.ListProbeValues;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,8 @@ import org.springframework.context.annotation.Import;
 import org.springframework.messaging.support.GenericMessage;
 import repo.ListProbeRepo;
 import org.springframework.messaging.Message;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 import static junit.framework.Assert.assertNull;
 import static org.mockito.Mockito.*;
@@ -27,6 +31,8 @@ public class AvgReducerTest {
     private static final long PROBE_ID_NO_VALUE = 125;
     private static final int VALUE = 100;
     private String bindingName = "";
+    private String producerBindingName = "avrProducer-out-0";
+    private String consumerBindingName = "avrConsumer-in-0";
     @Autowired
     InputDestination producer;
     @Autowired
@@ -51,6 +57,7 @@ public class AvgReducerTest {
         redisMap.put(PROBE_ID_NO_AVG, listProbeNoAvg);
     }
 
+    @Test
     void probeNoValuesTest() {
         when(listProbeRepo.findById(PROBE_ID_NO_VALUE)).thenReturn(Optional.ofNullable(null));
         when(listProbeRepo.save(new ListProbeValues(PROBE_ID_NO_VALUE))).thenAnswer(new Answer<ListProbeValues>() {
@@ -60,9 +67,46 @@ public class AvgReducerTest {
                 return invocation.getArgument(0);
             }
         });
-        producer.send(new GenericMessage<Probe>(probeNoValues));
+        producer.send(new GenericMessage<Probe>(probeNoValues), consumerBindingName);
 
-        Message<byte[]> message = consumer.receive(100, bindingName);
+        Message<byte[]> message = consumer.receive(100, producerBindingName);
         assertNull(message);
+        assertEquals(VALUE, redisMap.get(PROBE_ID_NO_VALUE).getValue().get(0));
+    }
+
+    @Test
+    void probeNoAvgTest() {
+        when(listProbeRepo.findById(PROBE_ID_NO_AVG)).thenReturn(Optional.ofNullable(listProbeNoAvg));
+        when(listProbeRepo.save(new ListProbeValues(PROBE_ID_NO_AVG))).thenAnswer(new Answer<ListProbeValues>() {
+            @Override
+            public ListProbeValues answer(InvocationOnMock invocation) throws Throwable {
+                redisMap.put(PROBE_ID_NO_AVG, invocation.getArgument(0));
+                return invocation.getArgument(0);
+            }
+        });
+        producer.send(new GenericMessage<Probe>(probeNoValues), consumerBindingName);
+
+        Message<byte[]> message = consumer.receive(100, producerBindingName);
+        assertNull(message);
+        assertEquals(VALUE, redisMap.get(PROBE_ID_NO_AVG).getValue().get(0));
+    }
+
+    @Test
+    void probeAvgTest() throws Exception {
+        when(listProbeRepo.findById(PROBE_ID_AVG)).thenReturn(Optional.ofNullable(listProbeAvg));
+        when(listProbeRepo.save(new ListProbeValues(PROBE_ID_AVG))).thenAnswer(new Answer<ListProbeValues>() {
+            @Override
+            public ListProbeValues answer(InvocationOnMock invocation) throws Throwable {
+                redisMap.put(PROBE_ID_AVG, invocation.getArgument(0));
+                return invocation.getArgument(0);
+            }
+        });
+        producer.send(new GenericMessage<Probe>(probeNoValues), consumerBindingName);
+
+        Message<byte[]> message = consumer.receive(100, producerBindingName);
+        assertNotNull(message);
+        ObjectMapper mapper = new ObjectMapper();
+        assertEquals(probeAvg, mapper.readValue(message.getPayload(), Probe.class));
+        assertEquals(VALUE, redisMap.get(PROBE_ID_AVG).getValue().get(0));
     }
 }
